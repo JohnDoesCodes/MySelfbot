3a86394224da2654d046b1cc7ff749a15250a4df
-apk <-apk>
2017-04-22 16:38:25 +0800

    .

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = (client, msg, args) => {
         startTime = Date.now();
-        channel.sendMessage("Pinging...").then((msg) => {
+        msg.channel.sendMessage("Pinging...").then((msg) => {
             endTime = Date.now();
             msg.edit(`Pong! \`${Math.round(endTime - startTime)}ms\``);
         });
