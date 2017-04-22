8c8831c907aabb0034b958adf6b38becd42fa948
-apk <-apk>
2017-04-22 16:55:58 +0800

    .

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,8 @@
+const client = new Discord.Client();
 module.exports = client => {
 	let games = [`with Coding`, `I love JavaScript`, `with Chris`, `xd`, `more xd`];
       setInterval(() => {
         client.user.setGame(games[Math.floor(Math.random() * games.length)])
-		console.log('I am ready!');
+		console.log('I am ready asf!');
 }, 12000)
 };
