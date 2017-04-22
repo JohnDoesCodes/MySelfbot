5baaa8500e945651811103c608e30b4ccd919508
-apk <-apk>
2017-04-22 12:16:31 +0800

    .

--- events/message.js
+++ events/message.js
@@ -1,8 +1,8 @@
 const config = require('../config.json');
 let bot = new Discord.Client();
 let Discord = require("discord.js");
-module.exports = message => {
-	if (!message.content.startsWith(config.prefix)) return;
+module.exports = msg => {
+	if (!msg.content.startsWith(config.prefix)) return;
   if (msg.author.id !== bot.user.id) return; //Only allows you to work with it, since it's called a selfbot :P
 
   const bot = msg.bot;
@@ -17,7 +17,7 @@ module.exports = message => {
 
 	try {
 		let cmdFile = require(`../commands/${command}`);
-		cmdFile.run(bot, message, args);
+		cmdFile.run(bot, msg, args);
 	} catch (err) {
 		console.log(`Command ${command} failed\n${err.stack}`);
 	}
