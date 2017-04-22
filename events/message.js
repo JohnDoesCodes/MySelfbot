73285aa28a96af774e902474f0a0fcbd834e6751
-apk <-apk>
2017-04-22 16:34:40 +0800

    .

--- events/message.js
+++ events/message.js
@@ -6,7 +6,13 @@ module.exports = msg => {
   const client = msg.client;
   const args = msg.content.split(' ');
   const command = args.shift().slice(config.prefix.length);
-
+  let prefix = config.prefix;
+    let channel = msg.channel;
+    let guild = msg.guild;
+    let text = msg.content;
+    let args = text.split(" ");
+    let command = text.substring(prefix.length, args[0].length).toLowerCase();
+    if (!msg.content.startsWith(prefix)) return;
 	try {
 		let cmdFile = require(`../commands/${command}`);
 		cmdFile.run(client, msg, args);
