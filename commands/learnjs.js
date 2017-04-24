156f81afbc6d0e8b2e57b16437ad99322123af3b
-apk <-apk>
2017-04-24 14:07:02 +0800

    .

--- commands/learnjs.js
+++ commands/learnjs.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-        if(message.author.client) return;
     var embed = new Discord.RichEmbed();
     embed.setFooter('Javascript Code', ' ')
     .addField('Learning JavaScript', "**__Useful links for learning JavaScript and Node__**:\n\n**Codecademy online course**: https://www.codecademy.com/learn/javascript\n**Eloquent Javascript, free book**: http://eloquentjavascript.net/\n\n**Some Node**:\nhttp://nodeschool.io/\nhttps://www.codeschool.com/courses/real-time-web-with-node-js\n**Discord.js getting started guides**:\nhttps://www.youtube.com/channel/UCvQubaJPD0D-PSokbd5DAiw/videos\nhttps://www.youtube.com/channel/UCLun-hgcYUgNvCCj4sIa-jA/videos\n**Javascript reference/docs**: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference\n**discord.js documentation**: https://discord.js.org/#!/docs")
