f1b4fe9e9e98a489da9484e94f97587f4472343c
-apk <-apk>
2017-04-24 13:46:02 +0800

    .

--- self.js
+++ self.js
@@ -60,23 +60,6 @@ exports.reload = reload;
 //             process.exit(1);
 //         });
 //     }
-
-//   if (message.content.startsWith(prefix + `si`)) {
-//     var embed = new Discord.RichEmbed();
-//     var moment = require('moment')
-//     embed.addField("> Server Info", `• Server name: ${message.guild.name}\n• Server ID: ${message.guild.id}\n• Owner: ${message.guild.owner.user.username}\n• Owner ID: ${message.guild.owner.id}\n• ${message.guild.memberCount} Members`, true)
-//         .addField('> Other', `• ${message.guild.roles.filter(r => r.name).size} roles\n• Region: ${message.guild.region}\n• ${message.guild.channels.filter(r => r.name).size} channels total count\n• Guild Created: ${moment(message.guild.createdAt).format('ddd MMM Do YYYY')}\n• Verification Level: ${message.guild.verificationLevel}`, true)
-//         .setColor("#EEFD3F")
-// 		.setFooter(' ', ' ')
-//         .setThumbnail(`${message.guild.iconURL}`) //`${message.guild.iconURL}`
-//         .setTimestamp()
-//         .setURL(`${message.author.avatarURL}`)
-//     message.channel.sendEmbed(
-//         embed, {
-//             disableEveryone: true
-//         }
-//     );
-//   }
   
 // if (message.content.toLowerCase().startsWith(prefix + "sb")) {
 //         var firstMention = message.guild.member(message.mentions.users.first());
