b867301c8a74341c8cb6a3d9e04293aedc8672fa
-apk <-apk>
2017-04-24 13:43:24 +0800

    .

---
+++ commands/w.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {
+ var mention = message.mentions.users.first();
+ if(message.mentions.users.size === 0) {
+return message.channel.sendMessage("\`?\` | Please mention a user.")
+ }
+  var embed = new Discord.RichEmbed();
+  var moment = require('moment')
+  embed.addField("> Member Details", `• Name: ${mention.username}\n• Discrim: #${mention.discriminator}\n• User ID: ${mention.id}\n• Game: ${mention.presence.game === null ? "No Game" : mention.presence.game.name}\n• Joined at: ${moment(mention.joinedAt).format('ddd MMM Do YYYY')}`, true)
+  .addField('> User Details', `• Status: ${mention.presence.status}\n• Joined Discord: ${moment(message.author.createdAt).format('ddd MMM Do YYYY')}`, true)
+  .setColor("#EEFD3F")
+  .setFooter(' ', ' ')
+  .setThumbnail(`${mention.avatarURL}`)//`${message.guild.iconURL}`
+  .setTimestamp()
+  .setURL(`${mention.avatarURL}`)
+message.channel.sendEmbed(
+  embed,
+  { disableEveryone: true }
+);
+};
--- self.js
+++ self.js
@@ -60,27 +60,6 @@ exports.reload = reload;
 //             process.exit(1);
 //         });
 //     }
-	
-//   if (command == "w") {
-//       var mention = message.mentions.users.first();
-//       if(message.mentions.users.size === 0) {
-//         return message.channel.sendMessage("\`?\` | Please mention a user.")
-//       }
-//   var embed = new Discord.RichEmbed();
-//   var moment = require('moment')
-//   embed.addField("> Member Details", `• Name: ${mention.username}\n• Discrim: #${mention.discriminator}\n• User ID: ${mention.id}\n• Game: ${mention.presence.game === null ? "No Game" : mention.presence.game.name}\n• Joined at: ${moment(mention.joinedAt).format('ddd MMM Do YYYY')}`, true)
-//   .addField('> User Details', `• Status: ${mention.presence.status}\n• Joined Discord: ${moment(message.author.createdAt).format('ddd MMM Do YYYY')}`, true)
-//   .setColor("#EEFD3F")
-//   .setFooter(' ', ' ')
-//   .setThumbnail(`${mention.avatarURL}`)//`${message.guild.iconURL}`
-//   .setTimestamp()
-//   .setURL(`${mention.avatarURL}`)
-// message.channel.sendEmbed(
-//   embed,
-//   { disableEveryone: true }
-// );
-// }
-
 
 //   if (message.content.startsWith(prefix + `si`)) {
 //     var embed = new Discord.RichEmbed();
