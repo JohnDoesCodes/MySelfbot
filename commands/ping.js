9f8ef9435e3c73d7cacb14a3550b7ecf92bb33df
-apk <-apk>
2017-04-24 12:56:59 +0800

    .

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,8 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-        message.channel.sendMessage("succ ma dicc");
+        startTime = Date.now();
+  message.channel.sendMessage("Pinging...").then((message) => {
+            endTime = Date.now();
+            message.edit(`Pong Succ ma dicc! \`${Math.round(endTime - startTime)}ms\``);
+  });
 }
