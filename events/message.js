52761be63eb00a117d6e3a81950faf428cf9d756
-apk <-apk>
2017-04-25 05:51:07 +0800

    .

--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ module.exports = message => {
   let client = message.client;
 	if (!message.content.startsWith(config.prefix)) return;
   if (message.author.id !== client.user.id) return;
-	let text = msg.content;
+	let text = message.content;
   const args = text.split(" ");
   let command = text.substring(prefix.length, args[0].length).toLowerCase();
 
