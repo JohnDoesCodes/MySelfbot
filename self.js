fa9fb4211ef79fc0ee23c921f714add9afb45fa1
-apk <-apk>
2017-04-24 13:14:33 +0800

    .

--- self.js
+++ self.js
@@ -61,25 +61,6 @@ exports.reload = reload;
 //         });
 //     }
 
-
-//  if (message.content === prefix + "stats") {
-//         var moment = require('moment')
-//         var embed = new Discord.RichEmbed();
-//         embed.setColor("#0099FF")
-//             .setFooter(' ', ' ')
-//             .setThumbnail(`${client.user.avatarURL}`)
-//             .setTimestamp()
-//             .addField('> Uptime', (Math.round(client.uptime / (1000 * 60 * 60))) + " hours, " + (Math.round(client.uptime / (1000 * 60)) % 60) + " minutes, and " + (Math.round(client.uptime / 1000) % 60) + " seconds")
-//             .addField('> General Stats', `• Servers: ${client.guilds.size}\n• Channels: ${client.channels.size}\n• Users: ${client.users.size}`, true)
-//             .addField('> Working on', `• New Pando\'s commands, click this [Link to join](https://discord.gg/dfdvArY)`)
-//             .addField('> Update', '• Added learnjs command')
-//         message.channel.sendEmbed(
-//             embed, {
-//                 disableEveryone: true
-//             }
-//         );
-// }
-
 //     if (command == "friend") {
 //         if (!message.mentions.users.first()) return message.edit('Failed to send friend request to a cool guy.');
 //         message.delete()
