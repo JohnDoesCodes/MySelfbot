f49942f20acf7f0125bb9b1f262c521baefaeae1
-apk <-apk>
2017-04-24 10:09:03 +0800

    .

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,5 @@
 const bot = new Discord.Client();
+const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
         startTime = Date.now();
         message.channel.sendMessage("Pinging...").then((message) => {
