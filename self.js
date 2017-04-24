553eef61cda2e184b9a2fd49d9747dc5969397e9
-apk <-apk>
2017-04-24 13:26:17 +0800

    .

--- self.js
+++ self.js
@@ -61,23 +61,6 @@ exports.reload = reload;
 //         });
 //     }
 
-//     if (command == "unblock") {
-//         if (!message.mentions.users.first()) return message.edit('Failed to unblocked a user, check you deym codes.');
-//         message.delete()
-//         message.mentions.users.first().unblock().then(() => {
-//             message.edit("A bitch has been unblocked.");
-//         })
-//     }
-
-// if (message.content.startsWith(prefix + "insult")) {
-//   if (!message.guild || !message.member) return;
-// let user = message.guild.member(message.mentions.users.first());
-// if (!user)
-// return message.channel.sendMessage("`io.insult <user> and try what can I do.`")
-//   var response = ['Youre a failed abortion whose birth certificate is an apology from the condom factory.', 'Your family tree is a cactus, because everybody on it is a prick.', 'It looks like your face caught on fire and someone tried to put it out with a fork.', 'Yow I have fuck your sister you son of a bitch!', 'You are so ugly that when your mama dropped you off at school she got a fine for littering.', 'I am fuckin your mother you fucking whore!', 'Your so ugly when you popped out the doctor said aww what a treasure and your mom said yeah lets bury it!', 'You were picking up trash yesterday but you forgot to pick up one thing: your skills', 'Your cant even beat me on 1v1, surely VentusGaming can make you suck your own dick!', 'Your Fuckin Noob', 'Yow Cyka Blyat you are a fuckin cunt that you are a son of your fuckin slut mother!'];
-// message.channel.sendMessage(response[~~(Math.random() * response.length)])
-// }
-
 //     if (message.content.toLowerCase().startsWith(prefix + 'quote') || message.content.toLowerCase().startsWith(prefix + 'q')) {
 
 //         var quote = message.content.split(" ").slice(1).join(" ");
