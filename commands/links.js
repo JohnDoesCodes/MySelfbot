bbbee0ac298743637cd686577684a3e5c771de97
-apk <-apk>
2017-04-26 11:37:13 +0800

    .

--- commands/links.js
+++ commands/links.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
         var embed = new Discord.RichEmbed();
         embed.setFooter('Johnツ#9897', ' ')
-    addField('My links', '[Click here](http://www.facebook.com/BenchMigami) to see my Facebook profile and send me a friend request. If you want to find me on discord just [click here](https://discord.gg/dfdvArY) to join my server. Here is my other server to join in, just [click here](https://discord.gg/8aKgYJx).')
+    .addField('My links', '[Click here](http://www.facebook.com/BenchMigami) to see my Facebook profile and send me a friend request. If you want to find me on discord just [click here](https://discord.gg/dfdvArY) to join my server. Here is my other server to join in, just [click here](https://discord.gg/8aKgYJx).')
     .setColor("#EEFD3F")
     message.channel.sendEmbed(
 
