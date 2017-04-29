285e237f1f4215402e71af94398d41e2c571565f
-apk <-apk>
2017-04-29 21:28:35 +0800

    .

--- commands/faq.js
+++ commands/faq.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
         message.delete()
     var embed = new Discord.RichEmbed();
     embed.setFooter('Code Made by Johnツ#9897', ' ')
-    .addField('Frequently Asked Questions', "// A bot under a user account. For example it is an userbot when others can use it. It becomes a selfbot when you put this line of code. `if (message.author.id !== client.user.id) return`")
+    .addField('Frequently Asked Questions', "A bot under a user account. For example it is an userbot when others can use it. It becomes a selfbot when you put this line of code. `if (message.author.id !== client.user.id) return`")
     .setColor("#EEFD3F")
     message.channel.sendEmbed(
         embed, {
