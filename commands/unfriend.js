75a9e945b81093c26d69ae53cec3b69252d2e521
-apk <-apk>
2017-04-29 20:14:53 +0800

    reload

--- commands/unfriend.js
+++ commands/unfriend.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
+    message.delete()
             if (!message.mentions.users.first()) return message.edit('Failed to unfriend a bitch, check your deym codes.');
-        message.delete()
         message.mentions.users.first().removeFriend().then(() => {
             message.edit("About time to remove this bitch from my friends list.");
         })
