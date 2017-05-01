0aa3c730557da6f324b6b0de08ad2bc84261c7a9
-apk <-apk>
2017-05-01 17:33:30 +0800

    .

--- commands/si.js
+++ commands/si.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
     message.delete()
         var embed = new Discord.RichEmbed();
     var moment = require('moment')
-    embed.addField("> Server Info", `• Server name: ${message.guild.name}\n• Server ID: ${message.guild.id}\n• Owner: ${message.guild.owner.displayName}\n• Owner ID: ${message.guild.owner.id}\n• ${message.guild.memberCount} Members`, true)
+    embed.addField("> Server Info", `• Server name: ${message.guild.name}\n• Server ID: ${message.guild.id}\n• Owner: ${message.guild.owner.name}\n• Owner ID: ${message.guild.owner.id}\n• ${message.guild.memberCount} Members`, true)
         .addField('> Other', `• ${message.guild.roles.filter(r => r.name).size} roles\n• Region: ${message.guild.region}\n• ${message.guild.channels.filter(r => r.name).size} channels total count\n• Guild Created: ${moment(message.guild.createdAt).format('ddd MMM Do YYYY')}\n• Verification Level: ${message.guild.verificationLevel}`, true)
         .setColor("#FF0000")
 		.setFooter(' ', ' ')
