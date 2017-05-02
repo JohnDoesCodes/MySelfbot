2e138e3ac2b9277ed7f973f83709c44b65874b9c
-apk <-apk>
2017-05-03 00:58:51 +0800

    .

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const config = require('../config.json');
 module.exports = client => {
-	let games = [`with Coding`, `I love JavaScript`, `with Chris`, `xd`, `more xd`];
+	let games = [`with Coding`, `I love JavaScript`, `xd`, `more xd`];
       setInterval(() => {
         client.user.setGame(games[Math.floor(Math.random() * games.length)])
 		console.log('I am ready asf!');
