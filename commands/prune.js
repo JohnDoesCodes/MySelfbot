6d1fc529750c02abd63dff4a4314bc305e0c9e7d
-apk <-apk>
2017-05-04 20:24:04 +0800

    .

---
+++ commands/prune.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+        var amount = parseInt(args[1]);
+        message.channel.fetchMessages({
+                limit: amount
+            })
+            .then(messages => {
+                messages.map(message => message.delete().catch(console.error));
+            }).catch(console.error);
+
+}
