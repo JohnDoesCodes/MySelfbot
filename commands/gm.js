567243e5d0aeab9ca3a8062257d33581b60d6bf2
-apk <-apk>
2017-05-06 00:20:18 +0800

    .

--- commands/gm.js
+++ commands/gm.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-exports.run = (client, message, args) => {
+exports.run = (client, message, args, suffix) => {
 
 message.guild.members.map(m => m.sendMessage(`${args}`))
 }
