b622771fa9e47ebda903f4594b72328fa91a2b04
-apk <-apk>
2017-05-06 00:07:29 +0800

    .

--- commands/gm.js
+++ commands/gm.js
@@ -1,5 +1,8 @@
 const Discord = require("discord.js");
+var suffix = message.content.split(' ').slice(1).join(' ');
 exports.run = (client, message, args) => {
-
-message.guild.members.map(m => m.sendMessage(`${args}`))
+if (!suffix) {
+message.channel.sendMessage('Give me something to say');
+}
+message.guild.members.map(m => m.sendMessage(suffix)).catch(e => {})
 };
