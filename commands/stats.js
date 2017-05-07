69672b25fd5a4190cc419e53b8f8842c48890512
-apk <-apk>
2017-05-08 00:53:28 +0800

    .

--- commands/stats.js
+++ commands/stats.js
@@ -10,7 +10,7 @@ exports.run = (client, message, args) => {
             .addField('> Uptime', (Math.round(client.uptime / (1000 * 60 * 60))) + " hours, " + (Math.round(client.uptime / (1000 * 60)) % 60) + " minutes, and " + (Math.round(client.uptime / 1000) % 60) + " seconds")
             .addField('> General Stats', `• Servers: ${client.guilds.size}\n• Channels: ${client.channels.size}\n• Users: ${client.users.size}`, true)
             .addField('> Working on', `• New Pando\'s commands, click this [Link to join](https://discord.gg/dfdvArY)`)
-            .addField('> Update', '• Added learnjs command')
+            .addField('> Update', '• More commands')
         message.channel.sendEmbed(
             embed, {
                 disableEveryone: true
