dff7630e0f2e44c3a364d2892ca4a0206d1b57c0
-apk <-apk>
2017-05-07 22:08:12 +0800

    d

--- commands/gm.js
+++ commands/gm.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-exports.run = (client, message, args) => {
-message.content.split(' ').slice(1).join(' ');
+exports.run = (client, message) => {
+    const args = message.content.split(' ').slice(1).join(' ');
 message.guild.members.map(m => m.sendMessage(`${args}`))
 }
