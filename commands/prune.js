a011df6650f64e08ecba2f0500afe02c2d2a7ac7
-apk <-apk>
2017-05-09 21:12:37 +0800

    .

---
+++ commands/prune.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+    let count = parseInt(args[0]) || 1;
+
+
+
+    message.delete();
+
+    message.channel.fetchMessages({ limit: Math.min(count, 100), before: message.id })
+
+        .then(messages => {
+
+            Promise.all(messages.map(m => m.delete()))
+
+                .catch(message.error)
+
+                .then(() => {
+
+                    message.channel.send(`\u2705 Purged \`${count}\` messages.`)
+
+                        .then(m => m.delete(2000));
+
+                });
+
+        }).catch(message.error);
+}
