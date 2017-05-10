d9b8b15bae1da59b4a6e10b822842810a5bef4cc
-apk <-apk>
2017-05-10 19:02:13 +0800

    .

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -2,6 +2,5 @@ const reqEvent = (event) => require(`../events/${event}`);
 const config = require('../config.json')
 module.exports = client => {
 client.on('ready', () => reqEvent('ready')(client));
-client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
 client.on('message', reqEvent('message'));
 };
