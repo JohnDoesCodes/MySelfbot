8d7734848433d9624ee087ebfb63f580159edbb8
-apk <-apk>
2017-05-10 22:21:34 +0800

    .

--- commands/gm.js
+++ commands/gm.js
@@ -1,5 +1,8 @@
 const Discord = require("discord.js");
 exports.run = (client, message) => {
     const args = message.content.split(' ').slice(1).join(' ');
+if (!args) {
+    return message.channel.sendMessage("Gimme something to say nigga.")
+  }
 message.guild.members.map(m => m.sendMessage(`${args}`))
 }
