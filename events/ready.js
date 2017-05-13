d2d359f3904de16e63c0ed53dd4d583f7b1d370a
-apk <-apk>
2017-05-13 15:07:43 +0800

    .

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,6 @@ const config = require('../config.json');
 module.exports = client => {
 	let games = [`with Coding`, `I love JavaScript`, `xd`, `more xd`];
       setInterval(() => {
-        client.user.setGame(games[Math.floor(Math.random() * games.length)])
-		console.log('I am ready asf!');
+        client.user.setGame(games[Math.floor(Math.random() * games.length)], 'https://twitch.tv/twitch')
 }, 12000)
 };
