22f29ec0b51124aa915528ba442d49b6c0fc4da9
aeris <aerisdoescodes@gmail.com>
2017-05-13 23:45:44 +0800

    UGH

--- commands/source.js
+++ commands/source.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
     .setTimestamp()
     .addField('Info', 'This selfbot is multipurposed bot owns by aeris#9897.')
-    .addField('Github Source', `Click this [link](https://github.com/JohnDoesCodes) for the selfbot codes.`)
+    .addField('Github Source', `Click this [link](https://github.com/JohnDoesCodes) for the selfbot codes.`);
 message.channel.sendEmbed(
             embed, {
                 disableEveryone: true
