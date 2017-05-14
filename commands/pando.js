140e2102eb1628830560fee2cb8238f87f5e8094
aeris <aerisdoescodes@gmail.com>
2017-05-14 10:32:18 +0800

    .

--- commands/pando.js
+++ commands/pando.js
@@ -5,7 +5,8 @@ exports.run = (client, message, args) => {
         embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
         .setTimestamp()
         .setColor("#53A6F3")
-        .addField('Pando\'s Hangout', "```xml\n<About us>\nThis guild is a help support for everyone that need help about Pando bot and the other main bots. We can discuss anything except againts the rules and mostly importantly have fun. Mainly, this server is used as a help server, where we talk about our main bots.\n```");
+        .addField('Pando\'s Hangout', "```xml\n<About us>\nThis guild is a help support for everyone that need help about Pando bot and the other main bots. We can discuss anything except againts the rules and mostly importantly have fun. Mainly, this server is used as a help server, where we talk about our main bots.\n```")
+        .addField('Invite Link', "https://discord.gg/dfdvArY");
         message.channel.sendEmbed(
             embed, {
                 disableEveryone: true
