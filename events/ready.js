b0ae73c2f6127203bdba57b7d77d5a9a54c6a73b
aeris <aerisdoescodes@gmail.com>
2017-05-15 13:51:19 +0800

    .

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const config = require('../config.json');
 module.exports = client => {
 	let games = [`with Coding`, `I love JavaScript`, `xd`, `more xd`];
       setInterval(() => {
-        client.user.setGame(games[Math.floor(Math.random() * games.length)], 'https://twitch.tv/twitch')
+        client.user.setGame(games[Math.floor(Math.random() * games.length)], 'https://twitch.tv')
 }, 12000)
 };
