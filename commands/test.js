628da4b7de370ce630a9120911f58073de593e47
aeris <aerisdoescodes@gmail.com>
2017-05-15 10:04:54 +0800

    0

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     var channel = message.channel.id
-    var message = fetchMessage
+    var message = message.id
     var ags = message.content.split(' ').slice(2).join(' ');
     if (!channel) return message.reply("Please give channel id")
     if (!message) return message.reply("Please give message id")
