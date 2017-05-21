93ee340e416b3b19f3cc47873e2538c129f879a3
aeris <aerisdoescodes@gmail.com>
2017-05-21 22:24:49 +0800

    .

--- commands/bots.js
+++ commands/bots.js
@@ -7,9 +7,9 @@ exports.run = (client, message, args) => {
         var embed = new Discord.RichEmbed();
         embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
         .setTimestamp()
-        .setThumbnail("https://cdn.discordapp.com/attachments/308137730141847552/313148092562145281/Pando.jpg")
+        .setThumbnail("https://cdn.discordapp.com/attachments/248014822082347008/315857237392359424/dog.jpg")
         .setColor("#53A6F3")
-        .addField('Pando\'s Hangout', "<About us>\nThis guild is a help support for everyone that need help about Pando bot and the other main bots. We can discuss anything except againts the rules and mostly importantly have fun. Mainly, this server is used as a help server, where we talk about our main bots.")
+        .addField('Bot Supports', "<About us>\nThis guild is a help support for everyone that need help about main bots. We can discuss anything except againts the rules and mostly importantly have fun. Mainly, this server is used as a help server, where we talk about our main bots.")
         .addField('Invite Link', "https://discord.gg/dfdvArY");
         message.channel.sendEmbed(
             embed, {
--- commands/cmds.js
+++ commands/cmds.js
@@ -1,39 +1,35 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    if (!message.guild || !message.member) return;
-   message.channel.sendMessage("Use `selfmcmds` or `selfncmds`.")
-};
-
 //     if (!message.guild || !message.member) return;
 //    message.channel.sendMessage(
 //    '```asciidoc' +
-//    '\n= Self bot Commands =' +
-//    '\nselfping         :: selfping will be responed by pong and ms' +
-//    '\nselfvac          :: selfvac vacation xD' +
-//    '\nselflearnjs      :: selflearnjs show\'s the how usefull links on leaning js' +
-//    '\nselflinks        :: selflinks show\'s my social media of John site and server' +
-//    '\nselfservers      :: selfservers show\'s the Verified Server\'s or the partnered server' +
+//    '\n= Moderation Commands =' +
 //    '\nselfprune        :: selfprune used to clear anywhere to 1-100 messages' +
-//    '\nselfuptime       :: selfuptime shows the uptime of the selfbot' +
-//    '\nselfreset        :: selfreset used to reset the selfbot' +
-//    '\nselfstats        :: selfstats used to show the selfbot stats' +
-//    '\nselffriend       :: selffriend <@user>  used to add friend' +
-//    '\nselfunfriend     :: selfunfriend <@user>  used to unfriend' +
-//    '\nselfblock        :: selfblock <@user>  used to block' +
-//    '\nselfunblock      :: selfunblock <@user>  used to unblock' +
-//    '\nselfquote        :: selfquote <message ID> used to quote message' +
-//    '\nselfinfo         :: selfinfo used to show user\'s info' +
-//    '\nselfw            :: selfw <@user> used to show user\'s info' +
-//    '\nselfsi           :: selfsi used to show server\'s info' +
-//    '\nselfembed        :: selfembed used for text embed <Bench role require>' +
+//    '\nselfclear        :: selfclear used to clear own message to 1-100 messages' +
+//    '\nselfgm           :: selfgm used to mass messages on a server' +
+//    '\nselfrestart      :: selfrestart used to restart. ' +
+//    '\nselfsearch       :: selfsearch used to searches a number of messages for some text' +
 //    '\nselfsoftban      :: selfsoftban <@user>  <reason required> used to softban user\'s' +
 //    '\nselfwarn         :: selfwarn <@user> <reason required> used to warn people' +
 //    '\nselfban          :: selfban <@user>  <reason required> used to ban users' +
 //    '\nselfunban        :: selfunban <@user> <reason required> used to unban users' +
 //    '\nselfkick         :: selfkick <@user>  <reason required> used to kick users' +
-//    '\nselfavi          :: selfavi used to show your avatar' +
-//    '\nselfget          :: selfget <@user> used to get user\'s avatar' +
-//    '\nselfcb           :: selfcb dont feel like explaining this one' +
 //    '\nselfreload       :: selfreload used to reload the bot by Developer (Owner)' +
 //    '\nselfeval         :: selfeval used for Owner only```'
 //    );
+// };
+
+
+  var embed = new Discord.RichEmbed();
+  embed.setAuthor('Commands', `${message.author.displayAvatarURL}`)
+  .addField('Moderation', '[selfban](https://discord.gg/dfdvArY) - Ban a user.\n[selfsoftban](https://discord.gg/dfdvArY) - softban a user.\n[selfkick](https://discord.gg/dfdvArY) - Kick a user.\n[selfwarn](https://discord.gg/dfdvArY) - Warn a user.\n[selfmute](https://discord.gg/dfdvArY) - Mute a user.\n[selfunmute](https://discord.gg/dfdvArY) - Unmute a muted user.\n[selfunban](https://discord.gg/dfdvArY) - Unban a user.\n[selfprune](https://discord.gg/dfdvArY) - Purge messages.\n[selfclear](https://discord.gg/dfdvArY) - Clear own message.')
+  .addField('Misc', '[selfsource](https://discord.gg/dfdvArY) - Selfbot source code.\n[selfbots](https://discord.gg/dfdvArY) - Bot supports.\n[selfsearch](https://discord.gg/dfdvArY) - Search message.\n[selfping](https://discord.gg/dfdvArY) - Check if selfbot is alive/latency.\n[selfsbinfo](https://discord.gg/dfdvArY) - FAQ about selfbots.\n[selfcasb](https://discord.gg/dfdvArY) - Creating a selfbot.\n[selfshowcodes](https://discord.gg/dfdvArY) - Show all codes.\n[selftoken](https://discord.gg/dfdvArY) - Info on user token.\n[selfpp](https://discord.gg/dfdvArY) - 8 lang how to save the princess.\n[selfvac](https://discord.gg/dfdvArY) - Vacation.\n[selflearnjs](https://discord.gg/dfdvArY) - Show useful link\'s on learning js.\n[selflinks](https://discord.gg/dfdvArY) - Show\'s my social media site and server.\n[selfstats](https://discord.gg/dfdvArY) - Show selfbot stats.\n[selffriend](https://discord.gg/dfdvArY) - Add friend a user\n[selfunfriend](https://discord.gg/dfdvArY) - Unfriend a user.\n[selfblock](https://discord.gg/dfdvArY) - Block a user.\n[selfunblock](https://discord.gg/dfdvArY) - Unblock a user.\n[selfquote](https://discord.gg/dfdvArY) - Quote a message.\n[selfinfo](https://discord.gg/dfdvArY) - Show userinfo.\n[selfw](https://discord.gg/dfdvArY) - Show\'s user\'s info.\n[selfsi](https://discord.gg/dfdvArY) - Server info\n[selfembed](https://discord.gg/dfdvArY) - Text embed.\n[selfavi](https://discord.gg/dfdvArY) - Show current avatar.\n[selfget](https://discord.gg/dfdvArY) - Get user\'s avatar.\n[selfcb](https://discord.gg/dfdvArY) - CodeBlock.')
+  .addField('Owner', '[selfeval](https://discord.gg/dfdvArY) - Runs javascript code.\n[selfrestart](https://discord.gg/dfdvArY) - Restart the selfbot.\n[selfgm](https://discord.gg/dfdvArY) - Message members in a server.')
+  message.author.sendEmbed(
+            embed, {
+                disableEveryone: true
+            }
+        ); {
+          message.react("📬")
+        }
+}
