da3e66e6eca3b1d77fa691b1a0b50b14dc6eaffd
aeris <aerisdoescodes@gmail.com>
2017-05-23 21:13:15 +0800

    .

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     var argresult = message.content.split(' ').slice(2).join(' ');
-    client.author.setGame(argresult); {
+    client.user.setGame(argresult); {
         return message.channel.send("Done!")
     }
 }
