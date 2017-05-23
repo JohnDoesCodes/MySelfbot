b3ae79a8ddb7a718e1c7c620eefa521d633506a3
aeris <aerisdoescodes@gmail.com>
2017-05-23 21:19:32 +0800

    .

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const config = require('../config.json');
 module.exports = (client, message) => {
 	// let games = [`with Coding`, `I love JavaScript`, `xd`, `more xd`];
   //     setInterval(() => {
-        client.user.setGame('JavaScript', 'https://twitch.tv/aeriscodes')
+        client.user.setGame('', 'https://twitch.tv/aeriscodes')
 // }, 12000)
 };
