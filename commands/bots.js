dd0d8e2e58bdb933f596f4c3baadf9bbd847e7bd
aeris <aerisdoescodes@gmail.com>
2017-05-27 21:37:38 +0800

    reload one more time

--- commands/bots.js
+++ commands/bots.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
         embed.setFooter("Bot Developers")
         .setColor("#53A6F3")
         .setTitle('Bot Supports')
-        .setDescription("<About us>\nThis guild is a help support for everyone that need help about main bots. We can discuss anything except againts the rules and mostly importantly have fun. Mainly, this server is used as a help server, where we talk about our main bots.")
+        .setDescription("This guild is a help support for everyone that need help about main bots. We can discuss anything except againts the rules and mostly importantly have fun. Mainly, this server is used as a help server, where we talk about our main bots.")
         .addField('Invite Link', "[Please click here](https://discord.gg/dfdvArY)")
         .setImage("https://cdn.discordapp.com/attachments/248014822082347008/315857237392359424/dog.jpg");
         message.channel.sendEmbed(
