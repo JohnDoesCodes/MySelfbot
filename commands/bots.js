4a31242eac47236a7faf717d3ce3efb686677f35
aeris <aerisdoescodes@gmail.com>
2017-05-28 22:18:38 +0800

    bot support

--- commands/bots.js
+++ commands/bots.js
@@ -6,8 +6,8 @@ exports.run = (client, message, args) => {
         }
     	var embed = new Discord.RichEmbed();
         embed.setColor("#53A6F3")
-        .setTitle('Bot Supports')
-        .setDescription("Aeris Bot Supports is a help support for everyone that need help about our main bots. We can discuss anything except againts the rules and mostly importantly have fun. Mainly, this server is used as a help server, where we talk about our main bots. We focus on tons of topics from gaming and coding, to NSFW and shitposting!")
+        .setAuthor('Aeris Discord!')
+        .addField("Info", "Aeris Bot Supports is a help support for everyone that need help about our main bots. We can discuss anything except againts the rules and mostly importantly have fun. Mainly, this server is used as a help server, where we talk about our main bots. We focus on tons of topics from gaming and coding, to NSFW and shitposting!")
         .addField('Official Server of ModBot!', "```fix\nWe have great bot developers! That means:\n```\n :black_small_square: We have our own discord bots\n :black_small_square: Great bot qualities\n :black_small_square: And much more!")
         .addField('Invite Link', "[Click Here](https://discord.gg/dfdvArY)")
         .addField('For Mobile', "https://discord.gg/dfdvArY")
