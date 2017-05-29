545d2841f82eaf1f4be63a240f57663b3dc81904
aeris <aerisdoescodes@gmail.com>
2017-05-29 16:50:56 +0800

    ..

--- commands/w.js
+++ commands/w.js
@@ -7,8 +7,8 @@ return message.channel.sendMessage("\`?\` | Please mention a user.")
  }
   var embed = new Discord.RichEmbed();
   var moment = require('moment')
-  embed.addField("> Member Details", `• Name: ${mention.username}\n• Discrim: #${mention.discriminator}\n• User ID: ${mention.id}\n• Game: ${mention.presence.game === null ? "No Game" : mention.presence.game.name}\n• Joined at: ${moment(mention.joinedAt).format('ddd MMM Do YYYY')}`, true)
-  .addField('> User Details', `• Status: ${mention.presence.status}\n• Joined Discord: ${moment(mention.createdAt).format('ddd MMM Do YYYY')}`, true)
+  embed.addField("> Member Details", `• Name: ${mention.username}\n• Discrim: #${mention.discriminator}\n• User ID: ${mention.id}\n• Game: ${mention.presence.game === null ? "No Game" : mention.presence.game.name}\n• Joined at: ${moment(mention.joinedAt).format('ddd MMM Do YYYY')}`)
+  .addField('> User Details', `• Status: ${mention.presence.status}\n• Joined Discord: ${moment(mention.createdAt).format('ddd MMM Do YYYY')}`)
   .setColor("#53A6F3")
   .setFooter(' ', ' ')
   .setThumbnail(`${mention.avatarURL}`)//`${message.guild.iconURL}`
