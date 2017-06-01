7be3768d7f3772b5eac145a5d9f43cf535a21fc4
aeris <aerisdoescodes@gmail.com>
2017-06-02 00:56:24 +0800

    ...

--- commands/pong.js
+++ commands/pong.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+message.delete()
 var msg1 = message.guild.members.map(m => m).join('\n')
 message.channel.send(msg1, {split: true, disableEveryone: true})
-message.delete()
 }
