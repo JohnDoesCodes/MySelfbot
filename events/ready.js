f1a56fc63715cc37351964227ad7d0bd5afcdb3f
aeris <aerisdoescodes@gmail.com>
2017-06-01 21:00:15 +0800

    ...

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
 const config = require('../config.json');
 module.exports = (client, message) => {
-	 let games = [`https://discord.io/aerisdiscord`, `https://discord.gg/dfdvArY`];
-       setInterval(() => {
-		   client.user.setGame(games[Math.floor(Math.random() * games.length)], 'https://twitch.tv/aeriscodes')
-        //client.user.setGame('JavaScript', 'https://twitch.tv/aeriscodes')
- }, 12000)
+	 //let games = [`https://discord.io/aerisdiscord`, `https://discord.gg/dfdvArY`];
+       //setInterval(() => {
+		//   client.user.setGame(games[Math.floor(Math.random() * games.length)], 'https://twitch.tv/aeriscodes')
+        client.user.setGame('JavaScript')
+ //}, 12000)
 };
