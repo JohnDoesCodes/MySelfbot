3b6c77058cf35974ed18dcb4cf67ba3109f83c5f
aeris <aerisdoescodes@gmail.com>
2017-06-02 20:43:19 +0800

    .

--- commands/test.js
+++ commands/test.js
@@ -8,23 +8,23 @@ message.delete();
 if(!option) 
 var embed = new Discord.RichEmbed();
 // embed.setColor(0xFF0e44)
-embed.setAuthor('Selfbot Help', 'Catagories')
+embed.setAuthor('Selfbot Help | Catagories')
 .addField('Utility', 'Utility commands', true)
 .addField('Moderation', 'Moderation Commands', true)
 .setDescription(`Type \`${prefix}\`help [catagory] for more help.`)
-.setThumbnail(client.user.displayAvatarURL)
+.setThumbnail(`${client.user.displayAvatarURL}`)
 .setFooter('Selfbot Made By Leera#0037')
 .setTimestamp();
 message.channel.send({ embed })
 if (option.toLowerCase() === "moderation") {
 var embed2 = new Discord.RichEmbed();    
 // embed2.setColor(0xFF0e44)
-embed.setAuthor('Selfbot Help', 'Sometext here')
+embed.setAuthor('Selfbot Help | Sometext here')
 .addField('Ping', 'Sends a response to the websocket and the latency back to the discord channel.', true)
 .addField('Purge - Clear', 'Purge messages in the channel. Clear your own messages in the channel.', true)
 .addField('Setgame - Stream', 'Set a game status. Set a streaming status', true)
 .addField('Eval', 'Evaluate code in JavaScript', true)
-.setThumbnail(client.user.displayAvatarURL)
+.setThumbnail(`${client.user.displayAvatarURL}`)
 .setFooter('Selfbot Made By Leera#0037')
 .setTimestamp();
 message.channel.send({ embed2 })
@@ -32,12 +32,12 @@ message.channel.send({ embed2 })
 if (option.toLowerCase() === "utility") {     
 var embed3 = new Discord.RichEmbed();    
 // embed3.setColor(0xFF0e44)
-embed.setAuthor('Selfbot Help', 'Sometext here')
+embed.setAuthor('Selfbot Help | Sometext here')
 .addField('Ping', 'Sends a response to the websocket and the latency back to the discord channel.', true)
 .addField('Purge - Clear', 'Purge messages in the channel. Clear your own messages in the channel.', true)
 .addField('Setgame - Stream', 'Set a game status. Set a streaming status', true)
 .addField('Eval', 'Evaluate code in JavaScript', true)
-.setThumbnail(client.user.displayAvatarURL)
+.setThumbnail(`${client.user.displayAvatarURL}`)
 .setFooter('Selfbot Made By Leera#0037')
 .setTimestamp();
 message.channel.send({ embed3 })    
@@ -45,12 +45,12 @@ message.channel.send({ embed3 })
 if (option.toLowerCase() === "test") {
 var embed4 = new Discord.RichEmbed();    
 // embed4.setColor(0xFF0e44)
-embed.setAuthor('Selfbot Help', 'Sometext here')
+embed.setAuthor('Selfbot Help | Sometext here')
 .addField('Ping', 'Sends a response to the websocket and the latency back to the discord channel.', true)
 .addField('Purge - Clear', 'Purge messages in the channel. Clear your own messages in the channel.', true)
 .addField('Setgame - Stream', 'Set a game status. Set a streaming status', true)
 .addField('Eval', 'Evaluate code in JavaScript', true)
-.setThumbnail(client.user.displayAvatarURL)
+.setThumbnail(`${client.user.displayAvatarURL}`)
 .setFooter('Selfbot Made By Leera#0037')
 .setTimestamp();
 message.channel.send({ embed4 })    
