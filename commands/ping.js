ed711f6564a69a7f005246ace959f1631b9fd8ae
aeris <aerisdoescodes@gmail.com>
2017-06-02 20:54:44 +0800

    ...

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   let option = message.content.split(' ').splice(1).join(' ');
   message.delete();
+  if(!option) 
         startTime = Date.now();
   message.channel.sendMessage("Pinging...").then((message) => {
             endTime = Date.now();
