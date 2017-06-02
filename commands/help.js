e4f1a4bf1781b9fa62cdc6d65957bdfe1f9d68e3
aeris <aerisdoescodes@gmail.com>
2017-06-03 00:39:43 +0800

    .

--- commands/help.js
+++
@@ -1,8 +0,0 @@
-    const Discord = require("discord.js");
-exports.run = (client, message, args) => {
-    let command = args[0];
-    if (client.commands(command)) {
-      command = client.commands.get(command);
-      message.channel.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}`);
-    }
-  }
