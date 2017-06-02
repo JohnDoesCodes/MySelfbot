1846ce64f025be23202fc332a30ff5f72371ada2
aeris <aerisdoescodes@gmail.com>
2017-06-02 21:06:37 +0800

    ...

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   let option = message.content.split(' ').splice(1).join(' ');
+
   message.delete();
   if(!option) 
-        startTime = Date.now();
   message.channel.sendMessage("Pong")
 if (option.toLowerCase() === "help") {
 message.channel.send("Ping: use to check if bot is alive.")
