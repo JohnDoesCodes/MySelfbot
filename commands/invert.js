bb9c11624d9120a8f97fcb2b8c7713e0a2b880ff
aeris <aerisdoescodes@gmail.com>
2017-06-04 19:56:44 +0800

    inviert

--- commands/invert.js
+++ commands/invert.js
@@ -4,10 +4,10 @@ exports.run = (client, message, args) => {
 if (message.mentions.users.size === 0) {
       const res = message.channel.send(':gear: generating...')
       Jimp.read(message.author.avatarURL, (err, avatar) => {
-        if (err) return res.edit('failed to generate.')
+        if (err) return message.edit('failed to generate.')
         avatar.invert()
         avatar.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-          res.delete()
+          message.delete()
           message.channel.send({
             files: [{
               attachment: buffer,
@@ -19,10 +19,10 @@ if (message.mentions.users.size === 0) {
     } else {
       const res = message.channel.send(':gear: generating...')
       Jimp.read(message.mentions.users.first().avatarURL, (err, avatar) => {
-        if (err) return res.edit('failed to generate.')
+        if (err) return message.edit('failed to generate.')
         avatar.invert()
         avatar.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-          res.delete()
+          message.delete()
           message.channel.send({
             files: [{
               attachment: buffer,
