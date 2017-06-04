eb37ced06aebc0a2765314ec20dbf14182c338d4
aeris <aerisdoescodes@gmail.com>
2017-06-04 19:20:41 +0800

    selfbot google

---
+++ commands/google.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+var request = require('request');
+exports.run = (client, message, args) => {
+let res = await message.channel.send(`:mag: \`${args}\`...`);
+      request(`https://www.googleapis.com/customsearch/v1?key=AIzaSyDu7_tL50kfEcegjXnYqfBxXrKqBrknkkY&cx=013036536707430787589:_pqjad5hr1a&q=${args}&alt=json`, function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+              var kek = JSON.parse(body)
+              //message.edit(`:mag: \`${args}\` **${kek.items[0].title}**\n__<${kek.items[0].link}>__\n${kek.items[0].snippet}`);
+              res.edit({embed: {
+                author: {name: `${args} - Google Search`, icon_url: `https://shady.world/assets/google.jpg`},
+                description: `[**${kek.items[0].title}**](${kek.items[0].link})\n\n${kek.items[0].snippet}`,
+                color: 0xffffff,
+                footer: {text: `${Number(kek.queries.request[0].totalResults).toLocaleString()} total results`}
+              }})
+          }
+      });
+}
