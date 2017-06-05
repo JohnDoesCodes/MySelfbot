ca838f626de3fe6a5fa9f2fda1c25f18f9cca1d2
aeris <aerisdoescodes@gmail.com>
2017-06-05 11:23:39 +0800

    xd

--- commands/discrim.js
+++ commands/discrim.js
@@ -2,4 +2,4 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 const res = client.users.filter(u => u.discriminator === `${args}`).map(u => u.username)
   if(res.length === 0) return message.channel.sendMessage(`:x: **No users found matching discriminator ${args}.**`)
-message.channel.sendMessage(`:mag_right: __**${res.length} users found with the discriminator ${args}**__:\n\n${res.join(', ')}`)}
+message.channel.sendMessage(`:mag_right: __**${res.length} users found with the discriminator ${args}**__:\n\n${res.join('\n')}`)}
