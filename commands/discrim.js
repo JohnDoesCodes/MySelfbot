704f44c6d2831b54644187deb6f542ea4f3adb3c
aeris <aerisdoescodes@gmail.com>
2017-06-05 22:13:46 +0800

    xd

--- commands/discrim.js
+++ commands/discrim.js
@@ -5,8 +5,8 @@ exports.run = (client, message, args) => {
 const res = client.users.filter(u => u.discriminator === `${args}`).map(u => u.username)
   if(res.length === 0) {
   embed.setDescription(`:x: **No users found matching discriminator ${args}.**`)
+  .setAuthor('aeris#0018 Selfbot | Discrim Search', client.user.displayAvatarURL)
   .setColor("#AB1212")
-  .setFooter(`$https://cdn.discordapp.com/attachments/306434811516682240/321289769424257024/Nepgear.png`, "aeris#0018 Selfbot")
   message.channel.sendEmbed(
             embed, {
                 disableEveryone: true
@@ -14,6 +14,7 @@ const res = client.users.filter(u => u.discriminator === `${args}`).map(u => u.u
         );
   } else {
         embed.setDescription(`:mag_right: __**${res.length} users found with the discriminator ${args}**__:\n\n${res.join('\n')}`)
+        .setAuthor('aeris#0018 Selfbot | Discrim Search', client.user.displayAvatarURL)
         .setColor("#2F79E2")
   message.channel.sendEmbed(
             embed, {
