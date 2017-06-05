a7a9ae7dc31e70e25ce7d54a32ed8387ca1dd344
aeris <aerisdoescodes@gmail.com>
2017-06-05 10:40:49 +0800

    xd

--- commands/urban.js
+++ commands/urban.js
@@ -5,14 +5,14 @@ exports.run = (client, message, args) => {
   .header("X-Mashape-Key", "kpkOWomvxOmshmL5UBLZYTjw7lWUp1LCls5jsnJhDLm4VjIPl6")
   .header("Accept", "text/plain")
   .end(function (result, err) {
-  if(!result.body.list[0]) return msg.reply('No definition found!')
-  //msg.channel.sendMessage(`\`Definition for ${args.join(' ')}\`\n\n**Definition**: ${result.body.list[0].definition}\n\n**Example**: ${result.body.list[0].example}\n\n**Author**: ${result.body.list[0].author}\n\n**Up / Down Ratio**: ${result.body.list[0].thumbs_up} :thumbsup: to ${result.body.list[0].thumbs_down} :thumbsdown:`)
+  if(!result.body.list[0]) return message.reply('No definition found!')
+  //message.channel.sendMessage(`\`Definition for ${args.join(' ')}\`\n\n**Definition**: ${result.body.list[0].definition}\n\n**Example**: ${result.body.list[0].example}\n\n**Author**: ${result.body.list[0].author}\n\n**Up / Down Ratio**: ${result.body.list[0].thumbs_up} :thumbsup: to ${result.body.list[0].thumbs_down} :thumbsdown:`)
   const embed = new(require('discord.js')).RichEmbed();
   embed.setTitle(`Definition for ${args}`)
   embed.setURL(`https://urbandictionary.com/`)
   embed.setDescription(`${result.body.list[0].definition}`)
   embed.addField(`Example`, `${result.body.list[0].example}`, false)
-  embet.setColor(0xffffff)
-  msg.channel.sendEmbed(embed)
+  embed.setColor(0xffffff)
+  message.channel.sendEmbed(embed)
 });
 } 
