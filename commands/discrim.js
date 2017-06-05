b127f95b40309d37de72edb8cb7e4947072089e9
aeris <aerisdoescodes@gmail.com>
2017-06-05 22:03:47 +0800

    xd

--- commands/discrim.js
+++ commands/discrim.js
@@ -1,5 +1,23 @@
 const Discord = require("discord.js");
+var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
+    message.delete()
 const res = client.users.filter(u => u.discriminator === `${args}`).map(u => u.username)
-  if(res.length === 0) return message.channel.sendMessage(`:x: **No users found matching discriminator ${args}.**`)
-message.channel.sendMessage(`:mag_right: __**${res.length} users found with the discriminator ${args}**__:\n\n${res.join('\n')}`)}
+  if(res.length === 0) {
+  embed.setDescription(`:x: **No users found matching discriminator ${args}.**`)
+  .setColor("#AB1212")
+  message.channel.sendEmbed(
+            embed, {
+                disableEveryone: true
+            }
+        );
+  } else {
+        embed.setDescription(`:mag_right: __**${res.length} users found with the discriminator ${args}**__:\n\n${res.join('\n')}`)
+        .setColor("#2F79E2")
+  message.channel.sendEmbed(
+            embed, {
+                disableEveryone: true
+            }
+        );
+  }
+}
