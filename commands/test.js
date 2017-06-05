677342e8c7df198fc728a06a7443691a18744e7a
aeris <aerisdoescodes@gmail.com>
2017-06-05 21:57:15 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -2,13 +2,14 @@ const Discord = require("discord.js");
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 const res = client.users.filter(u => u.discriminator === `${args}`).map(u => u.username)
-  if(res.length === 0) return 
+  if(res.length === 0) {
   embed.setDescription(`:x: **No users found matching discriminator ${args}.**`)
   message.channel.sendEmbed(
             embed, {
                 disableEveryone: true
             }
         );
+  }
         embed.setDescription(`:mag_right: __**${res.length} users found with the discriminator ${args}**__:\n\n${res.join('\n')}`)
   message.channel.sendEmbed(
             embed, {
