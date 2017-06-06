0c6fae8b76027b210ed82f2ca6819376dbe97546
aeris <aerisdoescodes@gmail.com>
2017-06-06 21:22:32 +0800

    ...

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 var embed = new Discord.RichEmbed();
 exports.run = (client, message) => {
+message.delete()
 const args = message.content.split(' ').slice(1).join(' ');
 client.user.setGame(`${args}`);
 embed.setDescription(`Your game has been set to \`${args}\``)
