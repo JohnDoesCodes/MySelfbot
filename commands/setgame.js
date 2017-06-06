f68a5413fae1d70d1681e3b8b878611e1a2d1e22
aeris <aerisdoescodes@gmail.com>
2017-06-06 21:16:19 +0800

    xd

--- commands/setgame.js
+++ commands/setgame.js
@@ -2,5 +2,12 @@ const Discord = require("discord.js");
 exports.run = (client, message) => {
 const args = message.content.split(' ').slice(1).join(' ');
 client.user.setGame(`${args}`);
-message.channel.send(`Your game has been set to \`${args}\``)
+embed.setDescription(`Your game has been set to \`${args}\``)
+  .setAuthor('aeris#0018 Selfbot | Game Status', client.user.displayAvatarURL)
+  .setColor("#AB1212")
+  message.channel.sendEmbed(
+            embed, {
+                disableEveryone: true
+            }
+        );
 }
