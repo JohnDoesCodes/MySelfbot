ca4ec965089c869ad6ab872106ef4b88f62c44ab
aeris <aerisdoescodes@gmail.com>
2017-06-06 21:12:07 +0800

    xd

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 exports.run = (client, message) => {
 const args = message.content.split(' ').slice(1).join(' ');
-client.user.setGame(`Your game has been set to \`${args}\``); {
+client.user.setGame(`${args}`); {
     if (!args) return message.channel.send("Please set something for your setgame status!")
 }
+message.channel.send(`Your game has been set to \`${args}\``)
 }
