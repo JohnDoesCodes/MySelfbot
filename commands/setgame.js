baa80b0d03309248fc14042e8da238f0c1d50cf2
aeris <aerisdoescodes@gmail.com>
2017-06-06 21:18:16 +0800

    xd

--- commands/setgame.js
+++ commands/setgame.js
@@ -5,8 +5,8 @@ const args = message.content.split(' ').slice(1).join(' ');
 client.user.setGame(`${args}`);
 embed.setDescription(`Your game has been set to \`${args}\``)
   .setAuthor('aeris#0018 Selfbot | Game Status', client.user.displayAvatarURL)
-  .setColor("#AB1212")
-  message.channel.sendEmbed(
+  .setColor("#2F79E2")
+  message.channel.send(
             embed, {
                 disableEveryone: true
             }
