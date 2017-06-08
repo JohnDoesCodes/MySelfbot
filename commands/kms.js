ead0a692ac0954123862619f6bd6cd2fd63b3602
aeris <aerisdoescodes@gmail.com>
2017-06-08 22:03:32 +0800

    xd

--- commands/kms.js
+++ commands/kms.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
+    message.delete()
     if (!message.guild.member(client.user).hasPermission("EMBED_LINKS")) {
             return message.channel.send("`ERROR:` I do not have permission to send Embed, contact administrator to get perm to send embed.");
         }
