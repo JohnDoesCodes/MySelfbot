d8b7d48b6df1ed3659c3e061fa37c595db48c58a
aeris <aerisdoescodes@gmail.com>
2017-06-08 22:02:27 +0800

    xd kms gif

--- commands/kms.js
+++ commands/kms.js
@@ -1,5 +1,9 @@
 const Discord = require("discord.js");
+var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
+    if (!message.guild.member(client.user).hasPermission("EMBED_LINKS")) {
+            return message.channel.send("`ERROR:` I do not have permission to send Embed, contact administrator to get perm to send embed.");
+        }
 embed.setColor("0x0099FF")
 .setAuthor("aeris#0018 Selfbot | GIF", client.user.displayAvatarURL)
 .setImage("https://media2.giphy.com/media/vFA7tz7WkEDXq/200.gif");
