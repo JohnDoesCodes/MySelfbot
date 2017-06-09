957937af0d26799ae6edd11992b9b35d18427d03
aeris <aerisdoescodes@gmail.com>
2017-06-09 16:36:45 +0800

    xd

---
+++ commands/b1nzy.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+var embed = new Discord.RichEmbed();
+exports.run = (client, message, args) => {
+    message.delete()
+    if (!message.guild.member(client.user).hasPermission("EMBED_LINKS")) {
+            return message.channel.send("`ERROR:` I do not have permission to send Embed, contact administrator to get perm to send embed.");
+        }
+embed.setColor("0x0099FF")
+.setAuthor("aeris#0018 Selfbot | GIF", client.user.displayAvatarURL)
+.setImage("https://ping-b1nzy.today/076c61.gif");
+message.channel.sendEmbed(
+            embed, {
+                disableEveryone: true
+            }
+        );
+}
