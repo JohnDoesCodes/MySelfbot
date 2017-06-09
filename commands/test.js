0ed99e55eb73a1143ad9f55ee9a29abbda67020a
aeris <aerisdoescodes@gmail.com>
2017-06-10 01:08:03 +0800

    xd

---
+++ commands/test.js
@@ -0,0 +1,50 @@
+const Discord = require('Discord.js');
+const randomcolor = require('randomcolor');
+
+exports.run = (client, message, args) => {
+
+    var user = message.mentions.users.first();
+
+    if(!user) {
+
+        message.delete();
+
+        var embed = new Discord.RichEmbed();
+
+        embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
+        .setDescription('Specify a user.')
+        .setColor(randomcolor())
+
+        message.channel.sendEmbed(
+
+          embed, {
+
+            disableEveryon: true
+
+          }
+
+        );
+
+    }
+
+var kill = ['CodingLife has been summoned and shot ', 'CodingLife has been summoned and brutally stabbed ', 'CodingLife has been and summoned hung ', 'CodingLife has been summoned and burned ', 'CodingLife has been summoned and sacrificed ', 'CodingLife has been summoned and beat '];
+
+        message.delete();
+
+        var embed = new Discord.RichEmbed();
+
+        embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
+        .setDescription(kill[~~(Math.random() - kill.length)] + user)
+        .setColor(randomcolor())
+
+        message.channel.sendEmbed(
+
+          embed, {
+
+            disableEveryon: true
+
+          }
+
+        );
+
+    }
