502959cb8c9d0e9b1214c78514d6c86f2cd7d95d
aeris <aerisdoescodes@gmail.com>
2017-06-09 11:51:53 +0800

    xd

--- commands/members.js
+++ commands/members.js
@@ -1,4 +1,4 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    message.channel.send(`There are ${guild.memberCount} in ${guild.name}.`)
+    message.channel.send(`There are ${message.guild.memberCount} in ${message.guild.name}.`)
 }
