d8faaafc40d5081cea9291ddd8bd2724f5f74e5a
aeris <aerisdoescodes@gmail.com>
2017-06-09 15:26:48 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -1,8 +1,12 @@
 const Discord = require("discord.js");
-const childProcess = require('child_process')
-exports.run = (client, message, args) => {
-childProcess.exec(message.originalContent, { shell: '/bin/bash' }, (err, stdout) => {
-      if (err) return message.channel.send(err.message, { code: true });
-      message.channel.send(stdout, { code: true });
+const request = require('request');
+exports.run = (message, args) => {
+const client = message.client;
+    message.guild.fetchMembers().then(() => {
+      const mods = message.guild.members.array()
+        .filter(m => !m.user.bot && client.util.isStaff(m))
+        .sort((a, b) => SORT_MAP[a.presence.status] - SORT_MAP[b.presence.status])
+        .map(m => `${STATUS_MAP[m.presence.status]} **${m.user.username}#${m.user.discriminator}**`);
+      message.channel.send([`Mods for **${message.guild.name}**`].concat(mods));
 });
 }
