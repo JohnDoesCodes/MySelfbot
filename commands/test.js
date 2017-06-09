476662cb66d6f835d2d0bf50df299f18cc2f7003
aeris <aerisdoescodes@gmail.com>
2017-06-09 15:27:26 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -1,3 +1,13 @@
+const STATUS_MAP = {
+  online: '<:vpOnline:212789758110334977>',
+  idle: '<:vpAway:212789859071426561>',
+  offline: '<:vpOffline:212790005943369728>',
+  dnd: '<:vpDnD:236744731088912384>',
+  streaming: '<:vpStreaming:212789640799846400>',
+};
+
+const SORT_MAP = { online: 1, idle: 2, streaming: 3, dnd: 4, offline: 5, invisible: 6 };
+
 const Discord = require("discord.js");
 const request = require('request');
 exports.run = (message, args) => {
