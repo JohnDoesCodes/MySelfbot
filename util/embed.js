a699a946bc23abb8d7302c9e665b53234346f422
aeris <aerisdoescodes@gmail.com>
2017-06-09 15:50:11 +0800

    xd

---
+++ util/embed.js
@@ -0,0 +1,15 @@
+module.exports = (url, title = url, description = 'ᅠ') => {
+  if (description !== 'ᅠ') description += '\n';
+  return {
+    title,
+    description: `${description}\n[Donate to keep Googlebot alive](https://discord.io/aerisdiscord)`,
+    url,
+    timestamp: new Date(),
+    video: { url },
+    image: { url },
+    footer: {
+      text: 'Powered by Google',
+      icon_url: 'https://google.com/favicon.ico',
+    },
+  };
+};
