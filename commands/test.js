74588f88185a511b027287adce8a68f8eef75564
aeris <aerisdoescodes@gmail.com>
2017-06-10 01:16:03 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,6 +3,8 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 
+var kill = ['CodingLife has been summoned and shot ', 'CodingLife has been summoned and brutally stabbed ', 'CodingLife has been and summoned hung ', 'CodingLife has been summoned and burned ', 'CodingLife has been summoned and sacrificed ', 'CodingLife has been summoned and beat '];
+
     var user = message.mentions.users.first();
 
     if(!user) {
@@ -24,13 +26,7 @@ exports.run = (client, message, args) => {
 
         );
 
-    }
-
-var kill = ['CodingLife has been summoned and shot ', 'CodingLife has been summoned and brutally stabbed ', 'CodingLife has been and summoned hung ', 'CodingLife has been summoned and burned ', 'CodingLife has been summoned and sacrificed ', 'CodingLife has been summoned and beat '];
-
-        message.delete();
-
-
+    } else {
         embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
         .setDescription(kill[~~(Math.random() - kill.length)] + user)
         .setColor(randomcolor())
@@ -44,5 +40,6 @@ var kill = ['CodingLife has been summoned and shot ', 'CodingLife has been summo
           }
 
         );
-
     }
+    
+}
