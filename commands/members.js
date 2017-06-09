6b4660c6fb38eee53231115f2e4e447116612f9b
aeris <aerisdoescodes@gmail.com>
2017-06-09 11:54:56 +0800

    ..

--- commands/members.js
+++ commands/members.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    message.channel.send(`There are ${message.guild.memberCount} members in ${message.guild.name}.`)
+    messsage.delete()
+    message.channel.send(`There are ${message.guild.memberCount} members in ${message.guild.name} and the owner is ${message.guild.owner.user.tag}.`)
 }
