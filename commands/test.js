3169e6c6882174dd933641095667171feec9c4f4
aeris <aerisdoescodes@gmail.com>
2017-06-10 01:18:21 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -1,45 +1,45 @@
-const Discord = require("discord.js");
-const randomcolor = require('randomcolor');
-var embed = new Discord.RichEmbed();
-exports.run = (client, message, args) => {
+// const Discord = require("discord.js");
+// const randomcolor = require('randomcolor');
+// var embed = new Discord.RichEmbed();
+// exports.run = (client, message, args) => {
 
-var kill = ['CodingLife has been summoned and shot ', 'CodingLife has been summoned and brutally stabbed ', 'CodingLife has been and summoned hung ', 'CodingLife has been summoned and burned ', 'CodingLife has been summoned and sacrificed ', 'CodingLife has been summoned and beat '];
+// var kill = ['CodingLife has been summoned and shot ', 'CodingLife has been summoned and brutally stabbed ', 'CodingLife has been and summoned hung ', 'CodingLife has been summoned and burned ', 'CodingLife has been summoned and sacrificed ', 'CodingLife has been summoned and beat '];
 
-    var user = message.mentions.users.first();
+//     var user = message.mentions.users.first();
 
-    if(!user) {
+//     if(!user) {
 
-        message.delete();
+//         message.delete();
 
 
-        embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
-        .setDescription('Specify a user.')
-        .setColor(randomcolor())
+//         embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
+//         .setDescription('Specify a user.')
+//         .setColor(randomcolor())
 
-        message.channel.sendEmbed(
+//         message.channel.sendEmbed(
 
-          embed, {
+//           embed, {
 
-            disableEveryon: true
+//             disableEveryon: true
 
-          }
+//           }
 
-        );
+//         );
 
-    } else {
-        embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
-        .setDescription(kill[Math.floor(Math.random() * kill.length)] + user)
-        .setColor(randomcolor())
+//     } else {
+//         embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
+//         .setDescription(kill[Math.floor(Math.random() * kill.length)] + user)
+//         .setColor(randomcolor())
 
-        message.channel.sendEmbed(
+//         message.channel.sendEmbed(
 
-          embed, {
+//           embed, {
 
-            disableEveryon: true
+//             disableEveryon: true
 
-          }
+//           }
 
-        );
-    }
+//         );
+//     }
     
-}
+// }
