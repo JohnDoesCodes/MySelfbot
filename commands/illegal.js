496b4dc1e42b75fbe32dc757d741d6201fc9bc0b
aeris <aerisdoescodes@gmail.com>
2017-06-11 14:59:57 +0800

    xd

---
+++ commands/illegal.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js");
+const request = require('request');
+exports.run = (client, message, args) => {
+messae.delete();
+message.channel.sendFile(`https://martmists.tk/api/v1/illegal?query=${args.split(" ").join('%20')}`, 'file.gif')
+}
