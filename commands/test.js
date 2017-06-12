94a4f625b1239b54e326d59c57e0cd192814680a
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:31:07 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -6,6 +6,6 @@ message.delete()
 embed.setAuthor("Kaos Krew", "https://cdn.discordapp.com/icons/310192676479959041/31e550fdac36ed0102fec739fd1cc0a2.jpg")
 .setDescription("Kaos Krew is a multi-platformed Gaming Community server with channels for everyone! We host Team Games aswell as events! Here you can meet new people, listen to music, discuss your favorite games, team up, and chill!")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/mxxbg8E")
+.addField("Server Invite", "https://discord.gg/kkuFuap")
 message.channel.send({embed})
 }
