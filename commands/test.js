fc709a68ce5920b809cf0154ee09068152c4e2d2
aeris <aerisdoescodes@gmail.com>
2017-06-12 13:56:45 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
-message.delete
+message.delete()
 embed.setAuthor("ΙԲ עﾷȗ ΔЯﾼ ЯﾼΔdΙﾢɢ ȚￃΙଌ ΙȚଌ Țﾷ ﾤΔȚﾼ lel :)")
 .setDescription("This server is basically one of its kind. The discord has friendly people that'll welcome you and treat you like a family. This certain discord has many different and unique roles that you'll never be bored. It has a diverse community and many bots for your enjoyment. So join today!!")
 .setColor(randomcolor())
