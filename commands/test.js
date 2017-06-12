4a4f9105eaeb39a6a1c8ec3bb546fdd8e0dcabf7
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:38:17 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("G/MATS", "https://cdn.discordapp.com/icons/273501028681515008/193b08ec00084239a2c4d632440446e8.jpg")
-.setDescription("There is also much more like Shared Origin, Uplay & Steam accounts. As well as a weekly give-away of 1 free Activation of Denuvo games!")
+embed.setAuthor("CrackingZone_Gaming", "https://cdn.discordapp.com/icons/273501028681515008/193b08ec00084239a2c4d632440446e8.jpg")
+.setDescription("Our Community Offers Uplay, Origin,Steam,Spotify,Netflix,Hulu premium accounts for free!!!\n\nWe also host 2 giveaways daily for our members!!!\n\nYou want more well we also offer direct links to HD movies and latest PC Games!!!!\n\nWe will keep you updated with latest gaming news and PC tech!!!")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/T6N2jB9")
+.addField("Server Invite", " https://discord.gg/3RajjsF")
 message.channel.send({embed})
 }
