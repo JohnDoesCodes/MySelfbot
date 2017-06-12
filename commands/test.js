4432b2aa49540bf67d96da096e92278db5aa4f5d
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:12:11 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("ChillZone", "https://cdn.discordapp.com/icons/219564597349318656/482922f8c6b9121069eaa442cfec2da8.jpg")
-.setDescription("A rapidly growing place to chill, meet new friends, and have fun. Everybody is welcome!")
+embed.setAuthor("United Pals", "https://cdn.discordapp.com/icons/284202358802612225/896c252efc4741cf2035e8a3a3db6d71.jpg")
+.setDescription("One of the most friendly, active and amazing discords out there! We have 200+ members and more friendly people come every single day :smile: Join now to become a part of this amazing community!")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/Qurr9Gv")
+.addField("Server Invite", "https://discord.gg/FASThWq")
 message.channel.send({embed})
 }
