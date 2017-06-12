bb7c89e84ad7379d87a58437ac18b4247b8f67a4
aeris <aerisdoescodes@gmail.com>
2017-06-12 21:58:04 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -7,7 +7,7 @@ embed.setAuthor("Discord News", "https://cdn.discordapp.com/icons/31047755601503
 .setDescription("Discord News is a server dedicated to bring you the latest Discord news as soon as it happens! We have a weekly newspaper with articles about Discord as well as a website which will post news as soon as it happens!")
 .addField(" Join our server now!", "[click here](https://discord.gg/NqYzzdv) | https://discord.gg/NqYzzdv", true)
 .addField("Check out our official server!", "[Click here](https://discordnews.ml/) | https://discordnews.ml/", true)
-.setImage("https://cdn.discordapp.com/icons/310477556015038465/b2478ee0ea497c4cade06c65cb79b92a.jpg")
+.setImage("https://images-ext-2.discordapp.net/external/Ncsq9y8njcnNnovHdPGPlxxf4Z58ABqjK2hZeeMydoI/https/cdn.discordapp.com/attachments/323285788764930048/323808047140962324/Discord_News_Banner.png")
 .setColor(randomcolor())
 message.channel.send({embed})
 }
