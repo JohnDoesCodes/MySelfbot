5843f89082dffcd6e756851d553904ca367f3b53
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:27:39 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor(" Atlantis", "https://cdn.discordapp.com/icons/282296936827191306/e2ff5da63db78e4db8139f2851f920f6.jpg")
-.setDescription("Growing community, with a friendly enviroment.\nCustom server bot, developed by staff members and the community members.\nEvents like movie night, game night, and much more!!\nOpen for anyone to join, and be themselves.\nFull of all sorts of people (Developers, Artists, Athletes, and more!)")
+embed.setAuthor("The Gay Squad!", "https://cdn.discordapp.com/icons/289482819347152896/b6bfe4e1bde7dd9411cd4039a13e4c2f.jpg")
+.setDescription("**__A place to be gay (or not) and enjoy the company of others!__**\n\nHere at the Gay Squad we have lots of events and a loving community!\nYou don't need to be gay to join, of course, so don't be scared!\n\nCome join us at the Gay Squad, you won't regret it! (:")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/CBUDnGw")
+.addField("Server Invite", "https://discord.gg/mxxbg8E")
 message.channel.send({embed})
 }
