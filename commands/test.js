120e6b0e11137a79f5bb79419c82f2ff3276dcaf
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:04:30 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,10 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("LGBTQ", "https://cdn.discordapp.com/icons/310157548244434947/c182a0438e1763cf98d1aafc2ea0e453.jpg")
-.setDescription("We welcome everyone, of every race, and gender. We do not judge, you are safe here. We support all sexualalities and interests. Come join today and make some friends!")
+embed.setAuthor("The Midnight Afterparty", "https://cdn.discordapp.com/icons/301398963192594432/724f45c829cfe81397e22b8a5b75745d.jpg")
+.setDescription("The Midnight Afterparty is dedicated to events such as karaoke/tournaments for various games. We are a group aiming to be a chill server where friends can enjoy our events aswell as talk to each other while fostering with other communities,We also do GFX Clashes for all those artists out ther, we also welcome all partnerships if they meet our requirements,so if you'd like to help, make sure to join:")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/AkeFgEa")
+.addField("Server Invite", "https://discord.gg/fC3vbQt", true)
+.addField("Website", "http://themidnightafterparty.tk/", true)
 message.channel.send({embed})
 }
