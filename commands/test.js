3e3ec5afe128b14bcb491a427e2d5e04aff30808
aeris <aerisdoescodes@gmail.com>
2017-06-12 16:10:01 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("ProCSS", "https://cdn.discordapp.com/attachments/309681105202708481/322368462276001793/ProCSS.png")
-.setDescription("The Discord for r/ProCSS, which is for moderators and subreddits who are in support of continued CSS use on Reddit.")
+embed.setAuthor("Night Sanity 2.0", "https://cdn.discordapp.com/attachments/309681105202708481/322368462276001793/ProCSS.png")
+.setDescription("Hello and Welcome to (Night Sanity 2.0) Discord Server.\n- We're are starting off fresh and new.  This is a FULLY (NSFW,Gaming,Socialize)server. This server is split into 3 full servers meaning you ma choose ful nsfw,gamin or socialize. We have all sorts of channels. We have few voice chats also do it yourself bots to help you learn about the server. We also do Giveaways, you can view that in the do it yourself bot. We plan to grow big within few days as i will be regathering from my old server.")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/Ym85STk")
+.addField("Server Invite", "https://discord.gg/nV3DCSH")
 message.channel.send({embed})
 }
