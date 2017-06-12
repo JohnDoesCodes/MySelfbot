aeba0bb29b51d083f0734b5c9e7f24524f173196
aeris <aerisdoescodes@gmail.com>
2017-06-12 15:07:13 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,10 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Rasberry Pie", "https://cdn.discordapp.com/icons/204621105720328193/6c44c0fcd3aa3864e77a66112584f99d.jpg")
-.setDescription("Raspberry Pi is a programming and tech discussion server. Join in and talk about anything from programming, to hardware, to games with over 1,700 members!")
+embed.setAuthor("Snail Racing", "https://cdn.discordapp.com/icons/204621105720328193/6c44c0fcd3aa3864e77a66112584f99d.jpg")
+.setDescription("Snail Racing is a Discord bot which allows you to run exciting races with people in your server, collect neat items, soar server item and level leaderboards, hoard rupees and earn admin-assigned roles. Watch out for traps and beat your server's members in snail races!")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/rQGMDJJ")
+.addField("Server Invite", "https://discord.gg/ZB82NjS")
+.addField("Bot Invite", "https://discordapp.com/oauth2/authorize?client_id=256556410031046657&scope=bot&permissions=19457")
 message.channel.send({embed})
 }
