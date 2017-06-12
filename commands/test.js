640eb6b9bab9b862a7a7f61bb87ed09b0d3b4b84
aeris <aerisdoescodes@gmail.com>
2017-06-12 16:17:26 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,10 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Gamer's Community", "https://cdn.discordapp.com/icons/306485688390647812/50ba7cdae1a9e4072635fe7c5584af33.jpg")
-.setDescription("A place to find friends, gamers and other interesting people to get along with.\nWe discuss about anything & everything, there's also cool activities for everyone. \nNeed any help? Well, there's always someone who could help you with whatever you need. \nWe have some of the best youtubers, gamers, developers, graphic designers, music producers and more. \nIf you're interested please feel free to join as we look forward in meeting new people")
+embed.setAuthor("AyyMD")
+.setDescription("Don't want to burn your house down with Novideo GPUs or Shintel CPUs? Then AyyMD is the right place for you! From dank memes to mocking silly Nvidiots, we have it all. We are a satirical PC hardware community dedicated to proving that AMD is clearly the better choice. Everyone is welcome, including non-AMD fanboys.")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/YFXycQZ")
+.addField("Server Invite", "https://discord.gg/AyyMD", true)
+.addField("Subreddit", "https://www.reddit.com/r/AyyMD/")
 message.channel.send({embed})
 }
