582b0796cfcdefd7187cf63c06cebe58bb22c270
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:59:20 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("The Chilled /D/iscord", "https://cdn.discordapp.com/attachments/304323891445104651/314592211188842496/test2.png")
-.setDescription("The Chilled /D/iscord is a community of a multitude of gamers from different games. Come together to laugh, share and enjoy music, involve yourself in activities of what the staff would organize, as well as share gaming techniques for a lot of games. This server was meant to be for all ages, but it has been decided for those who are mature enough to not get offended at a lot of 'seemingly' offensive topics.")
+embed.setAuthor("Teens Community", "https://cdn.discordapp.com/icons/306433739959828480/30b48cff2cd9fd66e7ed1329540c8b33.jpg")
+.setDescription("Hello and welcome to the teens community! :smiley: We hope you enjoy this server! Our main goal is to make this a server were everyone can have fun, socialize, meet others, talk to other teens who are going through the same things as you may be going through, support one another, and be crazy without regretting it :joy: We host plenty of events and have streams on Fridays. Come get in on the fun!")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/f6USW3B ")
+.addField("Server Invite", "https://discord.gg/zyAHdaJ")
 message.channel.send({embed})
 }
