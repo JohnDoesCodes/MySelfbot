dd8d0517a4b830c30640c380e548082e213fc4bf
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:55:32 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("The Onigiri Hub", "https://cdn.discordapp.com/icons/300834407185514496/f65ea54e9f5d1760b4e6fb9f8b099967.jpg")
-.setDescription("Welcome to The Onigiri Hub, here we have good customers and have many free roles for each kind of Onigiri. We also have some story tellers who tells different kinds of story, along with brilliant staff members. We have radio bots along with a roleplay and gaming channel. Partnership is offered here, so do join and tell us if you want to be one!")
+embed.setAuthor("The Chilled /D/iscord", "https://cdn.discordapp.com/attachments/304323891445104651/314592211188842496/test2.png")
+.setDescription("The Chilled /D/iscord is a community of a multitude of gamers from different games. Come together to laugh, share and enjoy music, involve yourself in activities of what the staff would organize, as well as share gaming techniques for a lot of games. This server was meant to be for all ages, but it has been decided for those who are mature enough to not get offended at a lot of 'seemingly' offensive topics.")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/PJCMCwV")
+.addField("Server Invite", "https://discord.gg/f6USW3B ")
 message.channel.send({embed})
 }
