7e0911b97b607bbf38ff1784ce81a9cbce71e964
aeris <aerisdoescodes@gmail.com>
2017-06-12 15:12:45 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Maid Cafe And Smoothie Shop", "https://cdn.discordapp.com/icons/299669140434845697/6abb0e9ca6bbdbf296ae4917bb6fe3ba.jpg")
-.setDescription("Chill out at the ever growing Maid Cafe! With fun events, role playing, and a lot of discussions. We are a server for all people, of all ages. This Maid Cafe aims to be one of the most active, growing servers that can be a place for all, free from mod abuse. Join today!")
+embed.setAuthor("Monkey Clubhouse", "https://cdn.discordapp.com/icons/289863074800861195/d8a127827f23e15f59b230e964cb5421.jpg")
+.setDescription("A gaming community. that plays League Of Legends, CSGO, Overwatch, Pubg. We have an Anime Community that is quite NSFW. Dont Worry if your not into the NSFW stuff we have a weeb area just for weebs ;). We are quite toxic. But if you want to have fun chatting shit to some guys/GRILLS across the internet. THIS IS THE PLACE! ")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/Mf6M9MT")
+.addField("Server Invite", "https://discord.gg/6NMcjpz")
 message.channel.send({embed})
 }
