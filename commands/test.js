6cc0dc6d498e6d09fa534248836e355d12cb3930
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:43:38 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Blocks Community", "https://cdn.discordapp.com/attachments/303099253260025858/310810037201141760/Blocks.png")
-.setDescription("Blocks community, a well growing community based on gamers, we support teams and games as well as minor communities! we have special bots(some are paid too) and reputed bots made for well moderation we have specific zones for specific nees like games chat, bots chat(yes we pay for cleverbots) and spam and nsfw too! this will be fun to join! why waiting? join and hangout with gamers!")
+embed.setAuthor("Chicks and Mix", "https://cdn.discordapp.com/icons/285391576417566721/1410b49f343cb2f24cd273dfe1a017f1.jpg")
+.setDescription("Hello and welcome! Chicks & Mix is fun and harmonious hangout originally created for chicks but to enjoy a good balance, we have opened our doors to create a mix. Ladies are chicks and gentlemen are mix. Please make yourself comfortable and have a fun and enjoyable time here.")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/CjNjRDZ")
+.addField("Server Invite", "https://discord.gg/2qg34Hn")
 message.channel.send({embed})
 }
