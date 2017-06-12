94c52223bc51ac48d8de62915617363f9f645b92
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:36:16 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("G/MATS", "https://cdn.discordapp.com/icons/303929603611820033/0b1016440c11098d38d6e3bc62d797e7.jpg")
+embed.setAuthor("G/MATS", "https://cdn.discordapp.com/icons/273501028681515008/193b08ec00084239a2c4d632440446e8.jpg")
 .setDescription("There is also much more like Shared Origin, Uplay & Steam accounts. As well as a weekly give-away of 1 free Activation of Denuvo games!")
 .setColor(randomcolor())
 .addField("Server Invite", "https://discord.gg/T6N2jB9")
