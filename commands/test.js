c42407cdf4bb89ec4679465e13f7b150cbe8b7fe
aeris <aerisdoescodes@gmail.com>
2017-06-12 15:52:22 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Ascension - Anime & Gaming", "https://cdn.discordapp.com/icons/315826908212232194/17761254284fe00979f99f5b899d55ce.jpg")
-.setDescription("The greatest discord group for Otakus. We want all of the anime/manga/jpop or etc. lovers to unite in one place.\nBy the way, if you want to have a role on our server, always be active on our chatroom. Mee6, a bot responsible for leveling people up, shall appear to let you know what level you are.")
+embed.setAuthor("LovehomePorn(+18)", "https://cdn.discordapp.com/attachments/309681105202708481/320935166430150656/logo.png")
+.setDescription("Be sure to join our Porn server, We have +70 channels  (Voice/chat)\n-Hentai\n-milf\n-gays and lesbians\n-anal\n-porn stories\nAnd MORE MORE...\nGiveaways and some some\nCome join and have fun get new friends\nWe trying to growing our Community\nJoin and Help us ")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/ZKWsdBY")
+.addField("Server Invite", "https://discord.gg/dVcWT6x")
 message.channel.send({embed})
 }
