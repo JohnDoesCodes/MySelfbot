70d48ead389972adc8f272c6fee663c199efd031
aeris <aerisdoescodes@gmail.com>
2017-06-12 14:47:12 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Chicks and Mix", "https://cdn.discordapp.com/icons/285391576417566721/1410b49f343cb2f24cd273dfe1a017f1.jpg")
-.setDescription("Hello and welcome! Chicks & Mix is fun and harmonious hangout originally created for chicks but to enjoy a good balance, we have opened our doors to create a mix. Ladies are chicks and gentlemen are mix. Please make yourself comfortable and have a fun and enjoyable time here.")
+embed.setAuthor("Coyote Cave | Coding HQ", "https://cdn.discordapp.com/icons/307230826545020928/f90e18ea95f15a955f30ba91a8dba1ba.jpg")
+.setDescription("Welcome to Coyote Cave | Coding HQ this is a chat for all coders around the world and a simple chat as well for my bot @Coyote | @CoyoteBeta.")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/2qg34Hn")
+.addField("Server Invite", "https://discord.gg/CYusa2Y")
 message.channel.send({embed})
 }
