010d36818da32e912e1f9a022823d75b0521091b
aeris <aerisdoescodes@gmail.com>
2017-06-12 15:23:04 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("GamerHub", "https://cdn.discordapp.com/icons/298684497581572097/2c6f8db9bcef16178d9ae4fb4d0dbd82.jpg")
-.setDescription("Need a place to chill, talk ,game and make youtube videos? Well you should join #PomfCraft! This server is an flabbergasting way to chill and meet new people :family_mmg: !\nWe also have an amazing bot made by a developer named FrogBot :frog: , FrogBot can do many cool commands + has nsfw commands!")
+embed.setAuthor("/r/Overwatch_Memes", "https://cdn.discordapp.com/icons/292932955653931009/81c10c3728d08c8651991fa621f676cb.jpg")
+.setDescription("Do you love overwatch and dank memes? Well, /r/Overwatch_Memes is here providing some of the dank memes for you. We hit 50K readers and also 100+ members on discord on the first day of the Overwatch Anniversary! ")
 .setColor(randomcolor())
-.addField("Server Invite", "https://discord.gg/RfzuBtw")
+.addField("Server Invite", "https://discord.gg/ow")
 message.channel.send({embed})
 }
