3ab61d8bae0cc6cf0859414746b7b3745687e450
aeris <aerisdoescodes@gmail.com>
2017-06-13 19:04:54 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -8,7 +8,7 @@ embed.setAuthor("Discord News", "https://cdn.discordapp.com/icons/31047755601503
 .addField(" Join our server now!", "[Click here](https://discord.gg/NqYzzdv)", true)
 .addField("Check out our official website!", "[Click here](https://discordnews.ml/)", true)
 .setImage("https://images-ext-2.discordapp.net/external/Ncsq9y8njcnNnovHdPGPlxxf4Z58ABqjK2hZeeMydoI/https/cdn.discordapp.com/attachments/323285788764930048/323808047140962324/Discord_News_Banner.png")
-.setFooter("Have fun ❤")
+.setFooter("Have fun ❤") 
 .setColor(randomcolor())
 message.channel.send({embed})
 }
