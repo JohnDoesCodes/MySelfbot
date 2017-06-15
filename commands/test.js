99df48bd4d7f6cb985f04be06cc2ff55e741b98e
aeris <aerisdoescodes@gmail.com>
2017-06-15 22:47:14 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,10 +3,10 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Center City", "https://cdn.discordapp.com/attachments/313493871193620481/324745528799657984/5b1d591de52dbc6135f9bfce99d00dc1.png")
-.setDescription("Welcome to Center City | Center City is a Role-playing and simulation based game that is currently getting developed and is created by the most amazing developers you can find. This game was designed to simulate real life events and to have fun and roleplay along with your friends. Center City is developed and is using Unity 3D to visualize real life simulation and competes with EmergeNYC. Center City has the best graphics and renderings of the textures inside of the gaming platform. You can even start a Center City self-hosted server and connect your server to Center City's databases.")
-.addField("Server Invite", "https://discord.gg/centercity")
-.addField("Website", "https://playcentercity.com")
+embed.setAuthor("Clash Royale Global (CRG)", "https://images-ext-2.discordapp.net/external/HG4E9uWzQjBijDwu8qV8zffnZcOOOVwuD5_YMiytr70/http/site-449644.mozfiles.com/files/449644/logo-1.png")
+.setDescription("Clash Royale Global is a discord server that's about all things clash royale! Ranging from Clash Royale artworks, strategy, clan recruitment, small youtubers and fun discussions! We host and scout many tournaments for the members of the server. Join now!")
+.setThumbnail("https://images-ext-1.discordapp.net/external/7YD_pHXGcVn7VkJ878hpQVcRR1Hv25kuRGMeTCmzVO4/http/vignette2.wikia.nocookie.net/clashroyale/images/7/76/Gg.png")
+.addField("Server Invite", "https://discord.gg/dwtEpKx")
 .setColor(randomcolor())
 message.channel.send({embed})
 } 
