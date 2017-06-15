0e3cc98653f05873538df54d45990d4adacb8526
aeris <aerisdoescodes@gmail.com>
2017-06-15 10:41:45 +0800

    xd

--- commands/perms.js
+++ commands/perms.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
     if (!message.guild || !message.member) return;
       message.channe.send('Your permissions are:\n' +
-        JSON.stringify(message.channel.permissionsFor(message.author).serialize(), null, 4));
+        message.channel.permissionsFor(message.author).serialize(), null, 4);
 }
