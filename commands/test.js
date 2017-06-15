c09271de1d1ee6fe8d14a1131365024ee658ee69
aeris <aerisdoescodes@gmail.com>
2017-06-15 11:03:11 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,9 +3,10 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Available Wrong Chat", "https://cdn.discordapp.com/icons/308776811741839370/6e4f4da6810ae294699b4809bc3f9876.jpg")
-.setDescription("Available Wrong Chat is a place to chill and hang out.\nEach role is given to a person based on how active they are with the server.\nYou can self assign roles to yourself with neat titles and what not.\nThe only thing the we won't be lenient with is other server links outside our partnership. \nSo please join and help get this community growing.")
-.addField("Server Invite", "https://discord.gg/RHUAhAU")
+embed.setAuthor("Center City", "https://cdn.discordapp.com/attachments/313493871193620481/324745528799657984/5b1d591de52dbc6135f9bfce99d00dc1.png")
+.setDescription("Welcome to Center City | Center City is a Role-playing and simulation based game that is currently getting developed and is created by the most amazing developers you can find. This game was designed to simulate real life events and to have fun and roleplay along with your friends. Center City is developed and is using Unity 3D to visualize real life simulation and competes with EmergeNYC. Center City has the best graphics and renderings of the textures inside of the gaming platform. You can even start a Center City self-hosted server and connect your server to Center City's databases.")
+.addField("Server Invite", "https://discord.gg/centercity")
+.addField("Website", "https://playcentercity.com")
 .setColor(randomcolor())
 message.channel.send({embed})
 } 
