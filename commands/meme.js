830d47e1b321f6193bc5b570fa561f2f880511da
aeris <aerisdoescodes@gmail.com>
2017-06-17 20:27:37 +0800

    xd

--- commands/meme.js
+++ commands/meme.js
@@ -3,8 +3,6 @@ const {get} = require("snekfetch");
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
     get("https://api.imgflip.com/get_memes").then(response => {
-        embed.setImage(response.body.file)
-        .setColor("#2FADE5")
-        message.channel.send({embed})
+        message.channel.send(response.body.file)
 });
 }
