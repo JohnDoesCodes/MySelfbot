22ce3f4aa28635b6953f555459345bd784a33d8e
aeris <aerisdoescodes@gmail.com>
2017-06-17 20:31:32 +0800

    xd

--- commands/meme.js
+++ commands/meme.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const {get} = require("snekfetch");
 var embed = new Discord.RichEmbed();
+let args = message.content.split(' ').slice(1);
 var result = args.join(' ');
 exports.run = (client, message, args) => {
         message.channel.send(`https://api.imgflip.com/get_memes?query=${result.split(" ").join('%20')}`)
