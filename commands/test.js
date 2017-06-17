dabdd7a136c7b86f35aeef29baeac1fc498d102c
aeris <aerisdoescodes@gmail.com>
2017-06-17 17:23:54 +0800

    xd

--- commands/test.js
+++ commands/test.js
@@ -3,10 +3,9 @@ const randomcolor = require('randomcolor');
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 message.delete()
-embed.setAuthor("Clash Royale Global (CRG)", "https://images-ext-2.discordapp.net/external/HG4E9uWzQjBijDwu8qV8zffnZcOOOVwuD5_YMiytr70/http/site-449644.mozfiles.com/files/449644/logo-1.png")
-.setDescription("Clash Royale Global is a discord server that's about all things clash royale! Ranging from Clash Royale artworks, strategy, clan recruitment, small youtubers and fun discussions! We host and scout many tournaments for the members of the server. Join now!")
-.setThumbnail("https://images-ext-1.discordapp.net/external/7YD_pHXGcVn7VkJ878hpQVcRR1Hv25kuRGMeTCmzVO4/http/vignette2.wikia.nocookie.net/clashroyale/images/7/76/Gg.png")
-.addField("Server Invite", "https://discord.gg/dwtEpKx")
+embed.setAuthor("Gamers House", "https://cdn.discordapp.com/icons/319618947894738961/0d68594fffa6459599cc31ee3af600ad.jpg")
+.setDescription("> Cool bots like Tatsumaki and Dyno!\n> and we allow advertising in our very own advertising channel!\n> we play a variety of games, on platforms such as steam, as well as playing mobile games too!\n> we partner with other servers, and we have 100+ members!\nWe have things such as:\n[> Friendly staff.Come .join!")
+.addField("Server Invite", "https://discord.gg/dPDXZpg")
 .setColor(randomcolor())
 message.channel.send({embed})
 } 
