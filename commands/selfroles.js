d02cb5c8ccde0a5e5267d7951ad4d254a4ced54a
aeris <aerisdoescodes@gmail.com>
2017-06-18 13:50:28 +0800

    xd

--- commands/selfroles.js
+++ commands/selfroles.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 var embed = new Discord.RichEmbed();
 embed.setAuthor("Colours", "https://cdn.discordapp.com/icons/280285147805384704/5e3a3f5a2a55921ccd7faa78386a3b4d.jpg")
-.setDescription("**__Red__**\n**__Cyan__**\n**__Black__**\n**__Pink__**\n**__Green__**\n**__Yellow__**")
+.setDescription("**__Red__**\n\n**__Cyan__**\n\n**__Black__**\n\n**__Pink__**\n\n**__Green__**\n\n**__Yellow__**")
 .setColor("#53A6F3")
 .setThumbnail("https://images-ext-2.discordapp.net/external/l1LE79QE8cU5wG7UMD9W-juq7SjA_4gbfbIzmFe87fA/https/www.prosci.com/media/wysiwyg/icons/icon-managers-dark.png?width=61&height=57")
 .setImage("https://images-ext-2.discordapp.net/external/Y4JhF7wcnzW3Qf9LP9lCzol0CNoiFaeTJe2TJdqZRRc/http/wallpapercave.com/wp/64FfDEi.jpg?width=300&height=188")
