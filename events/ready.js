a562c894438ff9e63fab0635d23bcf1685f00df6
aeris <aerisdoescodes@gmail.com>
2017-06-20 13:10:31 +0800

    xd

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,5 @@
-// const config = require('../config.json');
-// module.exports = (client, message) => {
-// 	 //let games = [`https://discord.io/aerisdiscord`, `https://discord.gg/dfdvArY`];
-//        //setInterval(() => {
-// 		//   client.user.setGame(games[Math.floor(Math.random() * games.length)], 'https://twitch.tv/aeriscodes')
-//         const args = message.content.split(' ').slice(1).join(' ');
-//         client.user.setGame(`${args}`)
-//  //}, 12000)
-// };
+const config = require('../config.json');
+module.exports = (client, message) => {
+  console.log(config)
+        client.user.setGame(`xd`)
+};
---
+++ github push.bat
@@ -0,0 +1,6 @@
+echo "# WelcomeBot" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git remote add origin https://github.com/JohnDoesCodes/Selfbot.git
+  git push -u origin master
