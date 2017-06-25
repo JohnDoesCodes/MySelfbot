74f5ce5a337770bfabcda24ca158f7076d835c0b
aeris <aerisdoescodes@gmail.com>
2017-06-25 11:19:30 +0800

    ...

--- commands/dog.js
+++ commands/dog.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const {get} = require("snekfetch");
 var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
-    get("https://random.dog/c65d2437-922f-4aed-80cf-39184b5c44b8.mp4").then(response => {
+    get("https://random.dog/woof").then(response => {
         embed.setImage(response.body.file)
         .setColor("#2FADE5")
         message.channel.send({embed})
