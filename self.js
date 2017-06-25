3f88342cff5363975f430fe7f33e7fddd522f222
aeris <patrickyamut@yahoo.com>
2017-06-25 18:12:51 +0800

    Add files via upload
    OwO

---
+++ self.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require('./config.json');
+// var randomcolor = require('randomcolor')
+var moment = require('moment')
+const fs = require('fs');
+var winston = require('winston')
+require('./util/eventLoader')(client);
+
+
+////////////////////////////////////////////
+//                                        //
+//                                        //
+//       Selfbot made by: aeris#0018      //
+//                                        //
+//                                        //
+//                                        //
+////////////////////////////////////////////
+
+process.on('uncaughtException', function(err) {
+    console.log('DUD I CAUGHT EXCEPTION?!??!: ' + err); //STOPS THE BOT FROM CRASHING
+});
+
+function AaN(args, i) {
+    if (args[i] === null || args[i] === "" || args[i] === undefined) return true;
+    return false;
+}
+
+const log = message => {
+    console.log("I\'m ready asf!");
+};
+
+var reload = (message, cmd) => {
+	delete require.cache[require.resolve('./commands/' + cmd)];
+	try {
+		let cmdFile = require('./commands/' + cmd);
+	} catch (err) {
+		message.channel.send(`Problem loading ${cmd}: ${err}`).then(
+			response => response.delete(1000).catch(error => console.log(error.stack))
+		).catch(error => console.log(error.stack));
+	}
+	message.channel.send(`${cmd} reload was a success!`).then(
+		response => response.delete(1000).catch(error => console.log(error.stack))
+	).catch(error => console.log(error.stack));
+};
+exports.reload = reload;
+
+client.login(config.tokens);
