b475bb6bd2e559effa90eba5b661e6baf3917be1
aeris <aerisdoescodes@gmail.com>
2017-06-25 11:25:47 +0800

    memes

--- commands/memes.js
+++ commands/memes.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 const {get} = require("snekfetch");
 var embed = new Discord.RichEmbed();
+let memes = "https://api.imgflip.com/get_memes"
 exports.run = (client, message, args) => {
-    get("https://random.dog/woof").then(response => {
+    get(memes).then(response => {
         embed.setImage(response.body.file)
         .setColor("#2FADE5")
         message.channel.send({embed})
