896416c7ed5b5e52c700b76a50cb178a08f4f2ed
aeris <aerisdoescodes@gmail.com>
2017-06-27 20:11:23 +0800

    lmfao

--- commands/myroles.js
+++ commands/myroles.js
@@ -3,7 +3,7 @@ var embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
     message.delete()
 embed.setAuthor("Colours", "https://cdn.discordapp.com/attachments/248014822082347008/322691939751297025/information.png")
-.setDescription("**__Cyan__**\n\n**__Red__**\n\n**__Blue__**\n\n**__Pink__**\n\n**__Black__**\n\n**__Green__**\n\n**__Lime__**\n\n**__Yellow__**\n\n**__Orange__**\n\n**__Purple__**\n\n**__Plum__**\n\n**__og(Ocean Blue)__**\n\n")
+.setDescription("**__Cyan__**\n\n**__Red__**\n\n**__Blue__**\n\n**__Pink__**\n\n**__Black__**\n\n**__Green__**\n\n**__Lime__**\n\n**__Yellow__**\n\n**__Orange__**\n\n**__Purple__**\n\n**__Plum__**\n\n**__OG__**\n\n")
 .setFooter("Type p!<colour> to apply for a colour; E.g, p!red. Type p!no<colour> to remove a role; e,g, p!nored.")
 .setThumbnail("https://cdn.discordapp.com/attachments/248014822082347008/329229145881968642/icon-managers-dark.png")
 .setImage("https://cdn.discordapp.com/attachments/248014822082347008/329229113380569088/64FfDEi.jpg")
