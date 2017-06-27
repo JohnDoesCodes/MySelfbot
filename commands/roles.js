0fbfde938ec37ecdd06166445204e84e46840a56
aeris <aerisdoescodes@gmail.com>
2017-06-28 06:35:37 +0800

    roles command is great

--- commands/roles.js
+++ commands/roles.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 var embed = new Discord.RichEmbed();
+if (!message.guild.member(client.user).hasPermission("EMBED_LINKS")) {
+        return message.channel.send("`ERROR:` I do not have permission to send Embed, contact administrator to get perm to send embed.");
+    }
 let roles = message.guild.roles.array().join(' | ');
 message.delete()
 embed.setAuthor(`${message.author.tag} Selfbot | Guild roles`, client.user.displayAvatarURL)
 .setDescription(roles)
 .setColor("#53A6F3")
 .setFooter(`There are ${message.guild.roles.filter(r => r.name).size} roles in ${message.guild.name}`)
-message.channel.send({embed})
+message.channel.send({embed}).catch((err) => {message.channel.send(`:warning: **An error occurred.**\n${err}`); console.log(err)});
 }
