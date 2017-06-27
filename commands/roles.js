91534a72ea7cc03f85f9adf8bebcc11c643ee530
aeris <aerisdoescodes@gmail.com>
2017-06-28 05:56:16 +0800

    ...

--- commands/roles.js
+++ commands/roles.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 var embed = new Discord.RichEmbed();
-let roles = message.guild.roles.array().join(' ');
+let roles = message.guild.roles.array().join('\n');
 message.delete()
 embed.setAuthor(`${message.author.tag} Selfbot | Guild roles`, client.user.displayAvatarURL)
 .setDescription(roles)
