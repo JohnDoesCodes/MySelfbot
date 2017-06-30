2b5b77e2933ddd0060736217f9d390e2280b2a6f
aeris <patrickyamut@yahoo.com>
2017-07-01 02:24:52 +0800

    Deleted files

--- commands/nick.js
+++
@@ -1,5 +0,0 @@
-const Discord = require("discord.js");
-exports.run = (client, message, args) => {
-	var suffix = message.content.split(' ').slice(1).join(' ');
-client.guilds.map(g => g.members.get(client.user.id).setNickname(suffix))
-}
