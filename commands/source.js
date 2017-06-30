82282a94879f3d6a9c7dc632fb91527868faf323
aeris <patrickyamut@yahoo.com>
2017-07-01 02:03:23 +0800

    [Source.js] Fixed .setColor("#53A6F3")

--- commands/source.js
+++ commands/source.js
@@ -8,7 +8,8 @@ exports.run = (client, message, args) => {
     embed.setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
     .setTimestamp()
     .addField('Info', 'This selfbot is multipurposed bot owns by aeris#0018.')
-    .addField('Github Source', `Click this [link](https://github.com/JohnDoesCodes) for the selfbot codes.`);
+    .addField('Github Source', `Click this [link](https://github.com/JohnDoesCodes/MySelfbot) for the selfbot codes.`);
+    .setColor("#53A6F3")
 message.channel.sendEmbed(
             embed, {
                 disableEveryone: true
