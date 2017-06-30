d0d9a8212d7c6742142a5bbb04e1dfb0ed23dba3
aeris <patrickyamut@yahoo.com>
2017-07-01 03:10:05 +0800

    Added [get.js] Remove [get2.js]

--- commands/get.js
+++ commands/get.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
         if (!message.guild || !message.member) return;
     var mention = message.mentions.users.first();
     if (!mention) {
-     return message.channel.send("\`❌\` | Mention a user.")
+     return message.channel.send("\`?\` | Mention a user.")
     }
-    console.log(mention.avatarURL)
+    message.channel.send(mention.avatarURL)
 };
