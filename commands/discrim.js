b4ff200725973fe006d0a433d6429f8e15cd30a8
aeris <aerisdoescodes@gmail.com>
2017-07-01 12:24:36 +0800

    ...

--- commands/discrim.js
+++ commands/discrim.js
@@ -6,7 +6,7 @@ let args = message.content.split(' ').splice(1).join(' ');
 const res = client.users.filter(u => u.discriminator === `${args}`).map(u => u.username);
   if(res.length === 0) {
 embed.setDescription(`No users found with ${args}`)
-.setColor(0xFF0ed4)
+.setColor("#53A6F3")
 .setTimestamp();
   message.channel.send({ embed })
   } else {
