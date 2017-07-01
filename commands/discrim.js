3ff577904912f767a400d6877739e4f9a3b99f48
aeris <aerisdoescodes@gmail.com>
2017-07-01 12:15:19 +0800

    added back discrim search

---
+++ commands/discrim.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+let embed = new Discord.RichEmbed();
+exports.run = (client, message, args) => {
+message.delete();
+let args = message.content.split(' ').splice(1).join(' ');
+const res = self.users.filter(u => u.discriminator === `${args}`).map(u => u.username);
+  if(res.length === 0) {
+embed.setDescription(`No users found with ${args}`)
+.setColor(0xFF0ed4)
+.setTimestamp();
+  message.channel.send({ embed })
+  } else {
+.setDescription(`**Users Found With ${args}**\n${res.join('\n')}`)
+.setColor("#53A6F3")
+.setTimestamp();
+message.channel.send({ embed })
+  }
+}
