928d5ca6c6b3367e2cbb947d32deefc5d2aa8e09
aeris <patrickyamut@yahoo.com>
2017-07-01 12:31:55 +0800

    [Discrim.js] - Added discrim search and fixed few errors

---
+++ commands/discrim.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+let embed = new Discord.RichEmbed();
+exports.run = (client, message) => {
+message.delete();
+let args = message.content.split(' ').splice(1).join(' ');
+const res = client.users.filter(u => u.discriminator === `${args}`).map(u => u.username);
+  if(res.length === 0) {
+embed.setAuthor(`No users found with ${args}`)
+.setColor("#53A6F3")
+.setTimestamp();
+  message.channel.send({ embed })
+  } else {
+embed.setAuthor(`Users Found With ${args}`)
+.setDescription(`${res.join('\n')}`)
+.setColor("#53A6F3")
+.setTimestamp();
+message.channel.send({ embed })
+  }
+}
