ba1ba059df704ae5909fb5f9812e1971c726aebc
aeris <aerisdoescodes@gmail.com>
2017-07-04 05:34:44 +0800

    .......

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
+
 const reqEvent = (event) => require(`../events/${event}`);
 const config = require('../config.json')
 module.exports = client => {
   client.on('ready', () => reqEvent('ready')(client));
   client.on('message', reqEvent('message'));
-=======
-const reqEvent = (event) => require(`../events/${event}`);
-const config = require('../config.json')
-module.exports = client => {
-  // client.on('ready', () => reqEvent('ready')(client));
-  client.on('message', reqEvent('message'));
->>>>>>> 68ae12d4ee819ce73c724b65761a3ab583bfc44a
+
 };
